a535226c6a5b3470ac8920dbe24f20a4340cbbeb
Hugo Lageneste <hugo.lageneste@dfinity.org>
2022-03-16 11:08:37 +0100

    Add video background to hero banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,14 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+    <header className={styles.heroBanner}>
+      <div className={styles.videoHero}>
+        <video className={styles.backgroundVideo} autoPlay muted loop playsInline id="background-video">
+          <source src="https://smartcontracts.org/video/internetComputer_small.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <div className={styles.videoContainer}>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
