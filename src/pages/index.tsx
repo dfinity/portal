a222eba7dcb49ce7b4758c1770abe19454ab3a46
Hugo Lageneste <hugo.lageneste@dfinity.org>
2022-03-16 11:16:11 +0100

    Remove default docs and update favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={siteConfig.title}
+      // TODO: change the desc
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
