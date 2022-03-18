ce8a829030a486333f23d59131c5fbd484126a59
Hugo Lageneste <hugo.lageneste@dfinity.org>
2022-03-18 10:34:51 +0100

    Initialize CI to get SDK references

---
+++ .github/workflows/tools/convert-asciidoc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for f in $(find $1 -name '*.adoc'); do 
+  asciidoctor -b docbook -a leveloffset=+1 -o - $f | \
+  pandoc --markdown-headings=atx --wrap=preserve -t markdown_strict -f docbook - > "${f%.adoc}.md"
+done
+
+find $1 -name "*.adoc" -type f -delete
