b43b92c1184e84af76322c52dabf58f5aa017f09
Hugo Lageneste <hugo.lageneste@dfinity.org>
2022-03-18 10:43:11 +0100

    Upgrade CI

--- .github/workflows/tools/convert-asciidoc.sh
+++ .github/workflows/tools/convert-asciidoc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 for f in $(find $1 -name '*.adoc'); do 
   asciidoctor -b docbook -a leveloffset=+1 -o - $f | \
   pandoc --markdown-headings=atx --wrap=preserve -t markdown_strict -f docbook - > "${f%.adoc}.md"
