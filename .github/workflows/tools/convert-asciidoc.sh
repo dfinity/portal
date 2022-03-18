07355845903d1f9dfeb29b6b5cbc189dc345c122
Hugo Lageneste <hugo.lageneste@dfinity.org>
2022-03-18 15:42:29 +0100

    Dump IC Protocol specification to references

--- .github/workflows/tools/convert-asciidoc.sh
+++ .github/workflows/tools/convert-asciidoc.sh
@@ -3,8 +3,8 @@
 set -e
 
 for f in $(find $1 -name '*.adoc'); do 
-  asciidoctor -b docbook -a leveloffset=+1 -o - $f | \
-  pandoc -t markdown_strict -f docbook - > "${f%.adoc}.md"
+  asciidoctor -b docbook -a leveloffset=+1 -o - "$f" | \
+  pandoc -t markdown_strict --wrap=none -f docbook - > "${f%.adoc}.md"
 done
 
 find $1 -name "*.adoc" -type f -delete
