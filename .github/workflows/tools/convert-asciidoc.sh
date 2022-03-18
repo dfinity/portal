3b94642954e55efbc6aaeec20b8134ae09739efc
Hugo Lageneste <hugo.lageneste@dfinity.org>
2022-03-18 10:46:14 +0100

    Remove some pandoc flags

--- .github/workflows/tools/convert-asciidoc.sh
+++ .github/workflows/tools/convert-asciidoc.sh
@@ -4,7 +4,7 @@ set -e
 
 for f in $(find $1 -name '*.adoc'); do 
   asciidoctor -b docbook -a leveloffset=+1 -o - $f | \
-  pandoc --markdown-headings=atx --wrap=preserve -t markdown_strict -f docbook - > "${f%.adoc}.md"
+  pandoc -t markdown_strict -f docbook - > "${f%.adoc}.md"
 done
 
 find $1 -name "*.adoc" -type f -delete
