bdc9d4cba2c53cc54431d4b4c8496e48947cf9ac
Hugo Lageneste <hugo.lageneste@dfinity.org>
2022-03-21 16:44:16 +0100

    Add new components on Landing page taken from smartcontracts.org

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -8,6 +8,12 @@ type FeatureItem = {
   description: JSX.Element;
 };
 
+type LinkItem = {
+  title: string;
+  description: JSX.Element;
+  href: string;
+}
+
 const FeatureList: FeatureItem[] = [
   {
     title: translate({ message: 'real web 3.0' }),
@@ -47,29 +53,90 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
+const LinkList: LinkItem[] = [
+  {
+    title: translate({ message: 'Sample Code' }),
+    description: (<>Learn how to build on the IC by exploring samples ranging from a simple DEX, to on-chain encrypted storage, NFT minting, and a basic DAO.</>),
+    href: ''
+  },
+  {
+    title: translate({ message: 'Quick Start' }),
+    description: (<>Get your first dapp launched on the IC in 20 minutes.</>),
+    href: ''
+  },
+  {
+    title: translate({ message: 'Community Forum' }),
+    description: (<>Engage with the IC community to shape future features, propose new ideas, and ask questions..</>),
+    href: ''
+  },
+  {
+    title: translate({ message: 'Wiki' }),
+    description: (<>Explore concepts, the architecture and technical breakthroughs that enable the IC. Find step-by-step guides such as how to stake your tokens.</>),
+    href: ''
+  }
+]
+
 function Feature({title, description}: FeatureItem) {
   return (
-    <div className={clsx('col col--3')}>
-      <div className="text--center padding-horiz--md">
-        <h3>{title.split(' ')[0]} <span className={styles.thin}>{title.split(' ').splice(1, title.length)}</span></h3>
-        <p style={{
-          color: 'gray'
-        }}>{description}</p>
-      </div>
+    <div className={clsx(styles.feature)}>
+      <h3>{title.split(' ')[0]} <span className={styles.thin}>{title.split(' ').splice(1, title.length)}</span></h3>
+      <p style={{
+        color: 'gray',
+        fontSize: '1em'
+      }}>{description}</p>
     </div>
   );
 }
 
+function Link({title, description, href}: LinkItem) {
+  return (
+    <div className={clsx(styles.link)}>
+      <h3 style={{
+        fontWeight: 'normal',
+        borderBottom: '1px solid #ABB1B3',
+        width: 'max-content',
+      }}>{title}</h3>
+      <p style={{
+        color: 'gray',
+        fontSize: '1em'
+      }}>{description}</p>
+    </div>
+  )
+}
+
 export default function HomepageFeatures(): JSX.Element {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+    <>
+      <section className={styles.links}>
+        <div className="container">
+          <div className="row">
+            {LinkList.map((props, idx) => (
+              <Link key={idx} {...props} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+
+        <h1 style={{
+          width: '50vw',
+          fontWeight: 'normal',
+          marginTop: '8rem',
+          marginBottom: '3rem'
+        }}>
+          <Translate>
+          After years of R&D, the Internet Computer blockchain is removing the limitations from smart contract software using advanced new cryptography. It scales its capacity with demand and can support unbounded volumes of smart contract computation, so that finally, dapps can scale. New “canister” smart contracts run super-fast and can even securely serve interactive web content directly to end users. On-chain costs have been crushed and are stable. Now code your contracts in any language that compiles to WebAssembly and create dapps and tokenized services for everything to chase the blockchain singularity.
+          </Translate>
+        </h1>
+      </section>
+
+      <section className={styles.features}>
+        <div className="container">
+          <div className="row">
+            {FeatureList.map((props, idx) => (
+              <Feature key={idx} {...props} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
