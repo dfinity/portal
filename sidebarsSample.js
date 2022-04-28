44609dd5ecdc94d09962f17bc1023bfdd852c93d
aisconnolly <aisling.connolly@dfinity.org>
2022-04-28 11:51:29 +0200

    tidy samples sidebar

--- sidebarsSample.js
+++ sidebarsSample.js
@@ -3,51 +3,29 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
     'samples': [
-        {
-            type: 'doc',
-            id: 'index',
-            label: 'Overview',
-        },
-        {
-            type: 'doc',
-            id: 'hello',
-            label: 'Hello World',
-        },
-        {
-            type: 'doc',
-            id: 'host-a-website',
-            label: 'Static Website',
-        },
-        {
-            type: 'doc',
-            id: 'dex',
-            label: 'Basic Dex',
-        },
-        {
-            type: 'doc',
-            id: 'nft',
-            label: 'NFT Minting',
-        },
-        {
-            type: 'doc',
-            id: 'dao',
-            label: 'Basic DAO',
-        },
-        {
-            type: 'doc',
-            id: 'encrypted-notes',
-            label: 'Encrypted Note-Taking',
-        },
-        {
-            type: 'doc',
-            id: 'token-transfer',
-            label: 'Token Transfer',
-        },
-        {
-            type: 'doc',
-            id: 'hackathon-projects',
-            label: 'Hackathon Projects',
-        },
+        'index',
+        'hello',
+        'host-a-website',
+        'dex',
+        'nft',
+        'dao',
+        'encrypted-notes',
+        'token-transfer',
+        'hackathon-projects',
+        // {
+        //     type: 'category',
+        //     label: 'Codelabs',
+        //     link: {
+        //       type: 'doc', id: 'codelabs/codelabs',
+        //     },
+        //     items: [
+        //       'codelabs/data-persistence',
+        //       'codelabs/minimalistic-motoko-dapp',
+        //       'codelabs/minimalistic-rust-dapp',
+        //       'codelabs/simple-nft',
+        //       'codelabs/static-website',
+        //     ],
+        //   },
     ],
 };
 module.exports = sidebars;
