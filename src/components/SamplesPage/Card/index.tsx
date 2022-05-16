db4f0d8367d8a22a935c841698a2f4432e004f74
Muriel Pinho <muriel.pinho@zuehlke.com>
2022-05-16 17:24:08 +0100

    Implemented card component for samples.

---
+++ src/components/SamplesPage/Card/index.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import styles from './index.module.css'
+import Link from "@docusaurus/Link";
+import MotokoIcon from "@site/static/img/samples/motoko.svg";
+import RustIcon from "@site/static/img/samples/rust.svg";
+import YoutubeIcon from "@site/static/img/samples/youtube.svg";
+import DocsIcon from "@site/static/img/samples/docs.svg";
+
+function MotokoLink() {
+    return <div className={styles.content}>
+        <div className={styles.iconLinkTooltipContainer}>
+            <span className={styles.iconLinkTooltip}>Motoko</span>
+        </div>
+        <Link className={styles.iconLink} to={"/"}><MotokoIcon/></Link>
+    </div>;
+}
+
+function RustLink() {
+    return <div className={styles.content}>
+        <div className={styles.iconLinkTooltipContainer}>
+            <span className={styles.iconLinkTooltip}>Rust</span>
+        </div>
+        <Link className={styles.iconLink} to={"/"}><RustIcon/></Link>
+    </div>;
+}
+
+function DocsLink() {
+    return <div className={styles.content}>
+        <div className={styles.iconLinkTooltipContainer}>
+            <span className={styles.iconLinkTooltip}>Developer Docs</span>
+        </div>
+        <Link className={styles.iconLink} to={"/"}><DocsIcon/></Link>
+    </div>;
+}
+
+function YoutubeLink() {
+    return <div className={styles.content}>
+        <div className={styles.iconLinkTooltipContainer}>
+            <span className={styles.iconLinkTooltip}>Watch Tutorials</span>
+        </div>
+        <Link className={styles.iconLink} to={"/"}><YoutubeIcon/></Link>
+    </div>;
+}
+
+function Index({image, title, domains, body, links}) {
+    return (
+        <div className={styles.container}>
+            <img src={image} className={styles.image} alt=""/>
+            <div className={styles.information}>
+                <div className={styles.mainDomain}>
+                    <span>{domains[0]}</span>
+                    <svg className={styles.domainDivider} width="1" height="12" viewBox="0 0 1 12" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <line x1="0.5" y1="12" x2="0.5" stroke="#181818" strokeOpacity="0.6"/>
+                    </svg>
+                    <span>{`+${domains.length - 1}`}</span>
+                    <div className={styles.domainsContainer}>
+                        <div className={styles.domains}>
+                            {domains.slice(1).map((domain) => <p key={domain}>{domain}</p>)}
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.header}>
+                    <p className={styles.title}>{title}</p>
+                    <Link className={styles.actionButton} to={links.action.to}>
+                        {links.action.text}
+                    </Link>
+                </div>
+                <p className={styles.body}>{body}</p>
+                <div className={styles.footer}>
+                    {links.motoko && <MotokoLink/>}
+                    {links.rust && <RustLink/>}
+                    {links.docs && <DocsLink/>}
+                    {links.youtube && <YoutubeLink/>}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Index;
--- src/pages/samples.tsx
+++ src/pages/samples.tsx
@@ -2,7 +2,9 @@ import React, {useEffect} from 'react';
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import styles from "@site/src/pages/samples.module.css";
-import Header from "@site/src/components/samplesPage/Header";
+import Header from "@site/src/components/SamplesPage/Header";
+import Card from "@site/src/components/SamplesPage/Card";
+import nft from "@site/static/img/samples/nft.png";
 
 function Samples(): JSX.Element {
     const {siteConfig} = useDocusaurusContext();
@@ -14,11 +16,42 @@ function Samples(): JSX.Element {
             title={siteConfig.title}
             description={siteConfig.tagline}>
             <main className={styles.main}>
-                <svg className={styles.BGShape} viewBox="0 0 10 10"
-                     xmlns="http://www.w3.org/2000/svg">
+                <svg className={styles.BGShape} viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
                     <circle fill="#3C01BA" cx="5" cy="5" r="5"/>
                 </svg>
                 <Header/>
+                <div className={styles.cards}>
+                    <Card image={nft} title={"NFT Minting"} domains={["Gaming", "Beginner", "NFT", "Motoko", "Rust"]}
+                          body={"Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus tellus ac cursus commodo tortor mauris condimentum."}
+                          links={{
+                              action: {text: "Get code", to: "/"},
+                              motoko: "/", rust: "/", docs: "/", youtube: "/"
+                          }}/>
+                    <Card image={nft} title={"NFT Minting"} domains={["Gaming", "Beginner", "NFT", "Motoko", "Rust"]}
+                          body={"Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus tellus ac cursus commodo tortor mauris condimentum."}
+                          links={{
+                              action: {text: "Get code", to: "/"},
+                              motoko: "/", rust: "/", docs: "/", youtube: "/"
+                          }}/>
+                    <Card image={nft} title={"NFT Minting"} domains={["Gaming", "Beginner", "NFT", "Motoko", "Rust"]}
+                          body={"Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus tellus ac cursus commodo tortor mauris condimentum."}
+                          links={{
+                              action: {text: "Get code", to: "/"},
+                              motoko: "/", rust: "/", docs: "/", youtube: "/"
+                          }}/>
+                    <Card image={nft} title={"NFT Minting"} domains={["Gaming", "Beginner", "NFT", "Motoko", "Rust"]}
+                          body={"Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus tellus ac cursus commodo tortor mauris condimentum."}
+                          links={{
+                              action: {text: "Get code", to: "/"},
+                              motoko: "/", rust: "/", docs: "/", youtube: "/"
+                          }}/>
+                    <Card image={nft} title={"NFT Minting"} domains={["Gaming", "Beginner", "NFT", "Motoko", "Rust"]}
+                          body={"Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus tellus ac cursus commodo tortor mauris condimentum."}
+                          links={{
+                              action: {text: "Get code", to: "/"},
+                              motoko: "/", rust: "/", docs: "/", youtube: "/"
+                          }}/>
+                </div>
             </main>
         </Layout>
     );
