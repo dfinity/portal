fff5c6bf16b9d40de8b79b6a7e79fc778ebf57b2
Muriel Pinho <muriel.pinho@zuehlke.com>
2022-05-18 12:52:00 +0100

    Finished filterbar design.

--- src/components/SamplesPage/FilterBar/index.tsx
+++ src/components/SamplesPage/FilterBar/index.tsx
@@ -1,44 +1,257 @@
-import React from 'react';
-import styles from './index.module.css';
+import React from "react";
+import styles from "./index.module.css";
 import ArrowDown from "@site/static/img/samples/arrowDown.svg";
 import ArrowUp from "@site/static/img/samples/arrowUp.svg";
 
-function Index() {
-    const [isSelectingLanguage, setIsSelectingLanguage] = React.useState(false);
-    const [selectedLanguages, setSelectedLanguages] = React.useState([]);
-    return (
-        <div className={styles.container}>
-            <span className={styles.title}>Filter Items</span>
-            <span className={styles.numberOfItems}>62</span>
-            <div className={styles.selectBox} style={{color: selectedLanguages.length > 0 ? "#3B00B9" : "black"}}
-                 onClick={() => setIsSelectingLanguage(!isSelectingLanguage)}>
-                <p className={styles.selectTitle}>Language</p>
-                <div className={styles.selectionArrow}>
-                    {isSelectingLanguage ? <ArrowUp/> : <ArrowDown/>}
-                </div>
-                {isSelectingLanguage &&
-                    <div className={styles.selectOptionsContainer}>
-                        <div className={styles.selectOptions}>
-                            <label className={styles.selectOption}>
-                                <input type="checkbox" value="motoko"/>
-                                Motoko
-                            </label>
-                            <label className={styles.selectOption}>
-                                <input type="checkbox" value="Rust"/>
-                                Rust
-                            </label>
-                            <label className={styles.selectOption}>
-                                <input type="checkbox" value="Javascript"/>
-                                Javascript
-                            </label>
-                        </div>
-                    </div>
-                }
-            </div>
+const languageOptions = ["Motoko", "Rust", "Javascript"];
+const domainOptions = [
+  "Global",
+  "Gaming",
+  "DeFi",
+  "Basic",
+  "Website",
+  "NFT",
+  "IoT",
+];
+const levelOptions = ["Beginner", "Intermediate", "Advanced"];
+const contentTypeOptions = ["Code Samples", "Videos", "Documentation"];
 
+function Index({
+  numberOfItems,
+  selectedLanguages,
+  setSelectedLanguages,
+  selectedDomains,
+  setSelectedDomains,
+  selectedLevels,
+  setSelectedLevels,
+  selectedContentTypes,
+  setSelectedContentTypes,
+}) {
+  const [isSelectingLanguage, setIsSelectingLanguage] = React.useState(false);
+  const [isSelectingDomain, setIsSelectingDomain] = React.useState(false);
+  const [isSelectingLevel, setIsSelectingLevel] = React.useState(false);
+  const [isSelectingContentType, setIsSelectingContentType] =
+    React.useState(false);
 
+  const updateCurrentSelection = (selection) => {
+    if (selection === "language") {
+      setIsSelectingLanguage(!isSelectingLanguage);
+      setIsSelectingDomain(false);
+      setIsSelectingLevel(false);
+      setIsSelectingContentType(false);
+    } else if (selection === "domain") {
+      setIsSelectingDomain(!isSelectingDomain);
+      setIsSelectingLanguage(false);
+      setIsSelectingLevel(false);
+      setIsSelectingContentType(false);
+    } else if (selection === "level") {
+      setIsSelectingLevel(!isSelectingLevel);
+      setIsSelectingDomain(false);
+      setIsSelectingLanguage(false);
+      setIsSelectingContentType(false);
+    } else if (selection === "contentType") {
+      setIsSelectingContentType(!isSelectingContentType);
+      setIsSelectingDomain(false);
+      setIsSelectingLevel(false);
+      setIsSelectingLanguage(false);
+    }
+  };
+
+  const clearFilters = () => {
+    setSelectedLanguages([]);
+    setSelectedDomains([]);
+    setSelectedLevels([]);
+    setSelectedContentTypes([]);
+    setIsSelectingLanguage(false);
+    setIsSelectingDomain(false);
+    setIsSelectingLevel(false);
+    setIsSelectingContentType(false);
+  };
+  const updateSelectedLanguages = (language) => {
+    if (selectedLanguages.includes(language)) {
+      setSelectedLanguages(
+        selectedLanguages.filter((item) => item !== language)
+      );
+    } else {
+      setSelectedLanguages([...selectedLanguages, language]);
+    }
+  };
+  const updateSelectedDomains = (domain) => {
+    if (selectedDomains.includes(domain)) {
+      setSelectedDomains(selectedDomains.filter((item) => item !== domain));
+    } else {
+      setSelectedDomains([...selectedDomains, domain]);
+    }
+  };
+  const updateSelectedLevels = (level) => {
+    if (selectedLevels.includes(level)) {
+      setSelectedLevels(selectedLevels.filter((item) => item !== level));
+    } else {
+      setSelectedLevels([...selectedLevels, level]);
+    }
+  };
+  const updateSelectedContentTypes = (contentType) => {
+    if (selectedContentTypes.includes(contentType)) {
+      setSelectedContentTypes(
+        selectedContentTypes.filter((item) => item !== contentType)
+      );
+    } else {
+      setSelectedContentTypes([...selectedContentTypes, contentType]);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <span className={styles.title}>Filter Items</span>
+      <span className={styles.numberOfItems}>{numberOfItems}</span>
+      <div className={styles.selectBoxContainer}>
+        <div
+          className={styles.selectBox}
+          style={{ color: selectedLanguages.length > 0 ? "#3B00B9" : "black" }}
+          onClick={() => updateCurrentSelection("language")}
+        >
+          <p className={styles.selectTitle}>Language</p>
+          <div className={styles.selectionArrow}>
+            {isSelectingLanguage ? <ArrowUp /> : <ArrowDown />}
+          </div>
         </div>
-    );
+        {isSelectingLanguage && (
+          <div className={styles.selectOptionsContainer}>
+            <div className={styles.selectOptions}>
+              {languageOptions.map((language) => (
+                <label className={styles.selectOption}>
+                  <input
+                    type="checkbox"
+                    key={language}
+                    value={language}
+                    checked={selectedLanguages.includes(language)}
+                    onChange={(e) => updateSelectedLanguages(e.target.value)}
+                  />
+                  {language}
+                </label>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+      <div className={styles.selectBoxContainer}>
+        <div
+          className={styles.selectBox}
+          style={{ color: selectedDomains.length > 0 ? "#3B00B9" : "black" }}
+          onClick={() => updateCurrentSelection("domain")}
+        >
+          <p className={styles.selectTitle}>Domain</p>
+          <div className={styles.selectionArrow}>
+            {isSelectingDomain ? <ArrowUp /> : <ArrowDown />}
+          </div>
+        </div>
+        {isSelectingDomain && (
+          <div className={styles.selectOptionsContainer}>
+            <div className={styles.selectOptions}>
+              {domainOptions.map((domain) => (
+                <label className={styles.selectOption}>
+                  <input
+                    type="checkbox"
+                    key={domain}
+                    value={domain}
+                    checked={selectedDomains.includes(domain)}
+                    onChange={(e) => updateSelectedDomains(e.target.value)}
+                  />
+                  {domain}
+                </label>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+      <div className={styles.selectBoxContainer}>
+        <div
+          className={styles.selectBox}
+          style={{ color: selectedLevels.length > 0 ? "#3B00B9" : "black" }}
+          onClick={() => updateCurrentSelection("level")}
+        >
+          <p className={styles.selectTitle}>Level</p>
+          <div className={styles.selectionArrow}>
+            {isSelectingLevel ? <ArrowUp /> : <ArrowDown />}
+          </div>
+        </div>
+        {isSelectingLevel && (
+          <div className={styles.selectOptionsContainer}>
+            <div className={styles.selectOptions}>
+              {levelOptions.map((level) => (
+                <label className={styles.selectOption}>
+                  <input
+                    type="checkbox"
+                    key={level}
+                    value={level}
+                    checked={selectedLevels.includes(level)}
+                    onChange={(e) => updateSelectedLevels(e.target.value)}
+                  />
+                  {level}
+                </label>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+      <div className={styles.selectBoxContainer}>
+        <div
+          className={styles.selectBox}
+          style={{
+            color: selectedContentTypes.length > 0 ? "#3B00B9" : "black",
+          }}
+          onClick={() => updateCurrentSelection("contentType")}
+        >
+          <p className={styles.selectTitle}>Content Type</p>
+          <div className={styles.selectionArrow}>
+            {isSelectingContentType ? <ArrowUp /> : <ArrowDown />}
+          </div>
+        </div>
+        {isSelectingContentType && (
+          <div className={styles.selectOptionsContainer}>
+            <div className={styles.selectOptions}>
+              {contentTypeOptions.map((contentType) => (
+                <label className={styles.selectOption}>
+                  <input
+                    type="checkbox"
+                    key={contentType}
+                    value={contentType}
+                    checked={selectedContentTypes.includes(contentType)}
+                    onChange={(e) => updateSelectedContentTypes(e.target.value)}
+                  />
+                  {contentType}
+                </label>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+      {(selectedLanguages.length > 0 ||
+        selectedDomains.length > 0 ||
+        selectedLevels.length > 0 ||
+        selectedContentTypes.length > 0) && (
+        <div onClick={() => clearFilters()} className={styles.clearFilters}>
+          Delete all filters
+          <svg
+            style={{ marginLeft: "6px" }}
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6 12C2.6862 12 0 9.3138 0 6C0 2.6862 2.6862 0 6 0C9.3138 0 12 2.6862 12 6C12 9.3138 9.3138 12 6 12ZM6 10.8C7.27304 10.8 8.49394 10.2943 9.39411 9.39411C10.2943 8.49394 10.8 7.27304 10.8 6C10.8 4.72696 10.2943 3.50606 9.39411 2.60589C8.49394 1.70571 7.27304 1.2 6 1.2C4.72696 1.2 3.50606 1.70571 2.60589 2.60589C1.70571 3.50606 1.2 4.72696 1.2 6C1.2 7.27304 1.70571 8.49394 2.60589 9.39411C3.50606 10.2943 4.72696 10.8 6 10.8ZM6 5.1516L7.6968 3.4542L8.5458 4.3032L6.8484 6L8.5458 7.6968L7.6968 8.5458L6 6.8484L4.3032 8.5458L3.4542 7.6968L5.1516 6L3.4542 4.3032L4.3032 3.4542L6 5.1516Z"
+              fill="currentColor"
+            />
+          </svg>
+        </div>
+      )}
+      <div className={styles.sortBy}>
+        <p>Sort By</p>
+        <div className={styles.sortByArrow}>{<ArrowDown />}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Index;
--- src/pages/samples.tsx
+++ src/pages/samples.tsx
@@ -1,62 +1,171 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import styles from "@site/src/pages/samples.module.css";
 import Header from "@site/src/components/SamplesPage/Header";
 import Card from "@site/src/components/SamplesPage/Card";
 import FilterBar from "@site/src/components/SamplesPage/FilterBar";
-import nft from "@site/static/img/samples/nft.png";
+import BGCircle from "@site/static/img/samples/bgcircle.svg";
+import nftMinting from "@site/static/img/samples/nftMinting.png";
+import helloWorld from "@site/static/img/samples/helloWorld.png";
+import staticWebsite from "@site/static/img/samples/staticWebsite.png";
+import basicDex from "@site/static/img/samples/basicDex.png";
+import basicDAO from "@site/static/img/samples/basicDAO.png";
+import encryptedNoteTaking from "@site/static/img/samples/encryptedNoteTaking.png";
+import tokenTransfer from "@site/static/img/samples/tokenTransfer.png";
+import actorReference from "@site/static/img/samples/actorReference.png";
+
+const sampleItems = [
+  {
+    title: "NFT Minting",
+    image: nftMinting,
+    domains: ["Gaming", "Beginner", "NFT", "Motoko", "Rust"],
+    body: "Create a user generated NFT and share it. This dapp uses the DIP721 NFT standard.",
+    links: {
+      action: { text: "Get code", to: "/" },
+      motoko: "/",
+      rust: "/",
+      docs: "/",
+      youtube: "/",
+    },
+  },
+  {
+    title: "Hello World",
+    image: helloWorld,
+    domains: ["Basic", "Motoko", "Rust"],
+    body: "Deploy a dead simple dapp using two canisters serving a web page.",
+    links: {
+      action: { text: "Get code", to: "/" },
+      motoko: "/",
+      rust: "/",
+      docs: "/",
+      youtube: "/",
+    },
+  },
+  {
+    title: "Static Website",
+    image: staticWebsite,
+    domains: ["Global", "Website", "Basic", "Motoko", "Rust"],
+    body: "Quickly set up a static website structure, add content and basic styling, and deploy on the IC.",
+    links: {
+      action: { text: "Docs", to: "/" },
+      motoko: "/",
+      rust: "/",
+      docs: "/",
+    },
+  },
+  {
+    title: "Basic Dex",
+    image: basicDex,
+    domains: ["DeFi", "Website", "Basic", "Motoko", "Rust"],
+    body: "Build dapp to enable DeFi applications on the IC.",
+    links: {
+      action: { text: "Get Code", to: "/" },
+      docs: "/",
+      youtube: "/",
+    },
+  },
+  {
+    title: "Basic DAO",
+    image: basicDAO,
+    domains: ["Gaming", "Website", "Basic", "Motoko", "Rust"],
+    body: "Dapp initializes a set of accounts and corresponding tokens as well as enables  proposals for communal votes.",
+    links: {
+      action: { text: "Get Code", to: "/" },
+      motoko: "/",
+      docs: "/",
+      youtube: "/",
+    },
+  },
+  {
+    title: "Encrypted note-taking",
+    image: encryptedNoteTaking,
+    domains: ["DeFi", "Website", "Basic", "Motoko", "Rust"],
+    body: "Create, access and modify confidential notes from multiple devices using Internet Identity and end-to-end encryption.",
+    links: {
+      action: { text: "Get Code", to: "/" },
+      rust: "/",
+      docs: "/",
+    },
+  },
+  {
+    title: "Token transfer",
+    image: tokenTransfer,
+    domains: ["Global", "Website", "Basic", "Motoko", "Rust"],
+    body: "Create a dapp that can transfer tokens to its most active users.",
+    links: {
+      action: { text: "Get Code", to: "/" },
+      motoko: "/",
+      rust: "/",
+      docs: "/",
+      youtube: "/",
+    },
+  },
+  {
+    title: "Actor reference",
+    image: actorReference,
+    domains: ["Basic", "Website", "Basic", "Motoko", "Rust"],
+    body: "Learn how the IC management canister functions as an actor (reference).",
+    links: {
+      action: { text: "Get Code", to: "/" },
+      rust: "/",
+      docs: "/",
+      youtube: "/",
+    },
+  },
+];
 
 function Samples(): JSX.Element {
-    const {siteConfig} = useDocusaurusContext();
-    useEffect(() => {
-        document.documentElement.style.setProperty('--ifm-color-primary', "#3b00b9");
-    }, []);
-    return (
-        <Layout
-            title={siteConfig.title}
-            description={siteConfig.tagline}>
-            <main className={styles.main}>
-                <svg className={styles.BGShape} viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-                    <circle fill="#3C01BA" cx="5" cy="5" r="5"/>
-                </svg>
-                <Header/>
-                <FilterBar />
-                <div className={styles.cards}>
-                    <Card image={nft} title={"NFT Minting"} domains={["Gaming", "Beginner", "NFT", "Motoko", "Rust"]}
-                          body={"Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus tellus ac cursus commodo tortor mauris condimentum."}
-                          links={{
-                              action: {text: "Get code", to: "/"},
-                              motoko: "/", rust: "/", docs: "/", youtube: "/"
-                          }}/>
-                    <Card image={nft} title={"NFT Minting"} domains={["Gaming", "Beginner", "NFT", "Motoko", "Rust"]}
-                          body={"Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus tellus ac cursus commodo tortor mauris condimentum."}
-                          links={{
-                              action: {text: "Get code", to: "/"},
-                              motoko: "/", rust: "/", docs: "/", youtube: "/"
-                          }}/>
-                    <Card image={nft} title={"NFT Minting"} domains={["Gaming", "Beginner", "NFT", "Motoko", "Rust"]}
-                          body={"Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus tellus ac cursus commodo tortor mauris condimentum."}
-                          links={{
-                              action: {text: "Get code", to: "/"},
-                              motoko: "/", rust: "/", docs: "/", youtube: "/"
-                          }}/>
-                    <Card image={nft} title={"NFT Minting"} domains={["Gaming", "Beginner", "NFT", "Motoko", "Rust"]}
-                          body={"Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus tellus ac cursus commodo tortor mauris condimentum."}
-                          links={{
-                              action: {text: "Get code", to: "/"},
-                              motoko: "/", rust: "/", docs: "/", youtube: "/"
-                          }}/>
-                    <Card image={nft} title={"NFT Minting"} domains={["Gaming", "Beginner", "NFT", "Motoko", "Rust"]}
-                          body={"Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus tellus ac cursus commodo tortor mauris condimentum."}
-                          links={{
-                              action: {text: "Get code", to: "/"},
-                              motoko: "/", rust: "/", docs: "/", youtube: "/"
-                          }}/>
-                </div>
-            </main>
-        </Layout>
+  const { siteConfig } = useDocusaurusContext();
+  const [selectedLanguages, setSelectedLanguages] = React.useState([]);
+  const [selectedDomains, setSelectedDomains] = React.useState([]);
+  const [selectedLevels, setSelectedLevels] = React.useState([]);
+  const [selectedContentTypes, setSelectedContentTypes] = React.useState([]);
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--ifm-color-primary",
+      "#3b00b9"
     );
+  }, []);
+  useEffect(() => {
+    console.log(selectedLanguages);
+  }, [selectedLanguages]);
+  useEffect(() => {
+    console.log(selectedDomains);
+  }, [selectedDomains]);
+
+  return (
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <BGCircle className={styles.BGShape} />
+          <Header />
+          <FilterBar
+            numberOfItems={sampleItems.length}
+            selectedLanguages={selectedLanguages}
+            setSelectedLanguages={setSelectedLanguages}
+            selectedDomains={selectedDomains}
+            setSelectedDomains={setSelectedDomains}
+            selectedLevels={selectedLevels}
+            setSelectedLevels={setSelectedLevels}
+            selectedContentTypes={selectedContentTypes}
+            setSelectedContentTypes={setSelectedContentTypes}
+          />
+          <div className={styles.cards}>
+            {sampleItems.map((sample) => (
+              <Card
+                image={sample.image}
+                title={sample.title}
+                domains={sample.domains}
+                body={sample.body}
+                links={sample.links}
+              />
+            ))}
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
 }
 
 export default Samples;
