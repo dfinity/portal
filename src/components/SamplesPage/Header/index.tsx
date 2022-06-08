918085f15b1f098ff1275d53bd5dfd02f068d2f6
Muriel Pinho <muriel.pinho@zuehlke.com>
2022-06-08 15:28:11 +0100

    updated examples repo link.

--- src/components/SamplesPage/Header/index.tsx
+++ src/components/SamplesPage/Header/index.tsx
@@ -9,7 +9,7 @@ function Index() {
       <span className={styles.body}>
         Get inspired! The possibilities of what to build on the IC are infinite.
         All samples and further projects can be found in the
-        <Link to={"/"}> examples repo</Link>.
+        <Link to={"https://github.com/dfinity/examples"}> examples repo</Link>.
       </span>
     </div>
   );
