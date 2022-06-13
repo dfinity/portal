057fc0da4240d5ddc8733eb1191aae8fe4a131c2
Severin Siffert <severin.siffert@dfinity.org>
2022-06-13 08:30:19 +0200

    rename II to not use the dapp label

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -131,7 +131,7 @@ const config = {
                 href: "https://wiki.internetcomputer.org/wiki/Internet_Computer_wiki#Introduction_to_the_Internet_Computer",
               },
               {
-                label: "Internet Identity dapp",
+                label: "Internet Identity",
                 href: "https://identity.ic0.app/",
               },
               {
@@ -251,15 +251,14 @@ const config = {
       announcementBar:
         isDev || isDeployPreview
           ? {
-              id: "local_dev",
-              content: isDeployPreview
-                ? `You are currently viewing a preview of this <a href="${
-                    process.env.PR_URL || "https://github.com/dfinity/portal"
-                  }">Pull Request</a>.`
-                : 'You are currently locally editing the Developer Portal. Contributing guidelines are available <a href="https://github.com/dfinity/portal#contributing">here</a>.',
-              textColor: "#091E42",
-              isCloseable: false,
-            }
+            id: "local_dev",
+            content: isDeployPreview
+              ? `You are currently viewing a preview of this <a href="${process.env.PR_URL || "https://github.com/dfinity/portal"
+              }">Pull Request</a>.`
+              : 'You are currently locally editing the Developer Portal. Contributing guidelines are available <a href="https://github.com/dfinity/portal#contributing">here</a>.',
+            textColor: "#091E42",
+            isCloseable: false,
+          }
           : undefined,
 
       footer: {
