c2bc8eb9a70629cd65ca3abcfd5b7d636734c640
Muriel Pinho <muriel.pinho@zuehlke.com>
2022-07-29 11:35:20 +0100

    Update padding in hero to be the same on all pages.

--- src/pages/how-it-works.tsx
+++ src/pages/how-it-works.tsx
@@ -22,7 +22,7 @@ function HowItWorks() {
             className="absolute pointer-events-none max-w-none w-[800px] -right-[370px] top-[-100px] md:w-[1500px]  md:right-[-700px] 2xl:left-1/2 translate-x-[200px] md:top-[-200px] z-[-1000]"
             variants={transitions.item}
           />
-          <section className="max-w-page relative px-6 pt-12 mb-5 md:mb-6 md:px-12.5 md:mx-auto md:pt-20 overflow-hidden">
+          <section className="max-w-page relative px-6 pt-20 mb-5 md:mb-6 md:px-12.5 md:mx-auto md:pt-40 overflow-hidden">
             <div className="md:w-7/10 lg:w-6/10 md:ml-1/12">
               <motion.h1
                 className="tw-heading-3 md:tw-heading-2"
