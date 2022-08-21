8a7c16ca61fcce01d06d38ab1f8615dfbffba532
Dominic Williams <dominicwilliams007@gmail.com>
2022-08-21 20:58:36 +0200

    Update index.tsx

--- src/components/LandingPage/HeroSection/index.tsx
+++ src/components/LandingPage/HeroSection/index.tsx
@@ -133,7 +133,7 @@ function Index() {
           className={styles.actionContainer}
         >
           <Link className={styles.actionButton} to="/developers">
-            BUILD REAL WEB3
+            CODE WEB3
           </Link>
           <Link className={styles.callToAction} to={"/showcase"}>
             Explore the Internet Computer ecosystem
