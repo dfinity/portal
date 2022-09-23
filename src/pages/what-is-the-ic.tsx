e9bf1542707b8f85784abe547b01979a3f2a54fe
aisconnolly <aisling.connolly@dfinity.org>
2022-09-23 17:26:03 +0200

    hero update

--- src/pages/what-is-the-ic.tsx
+++ src/pages/what-is-the-ic.tsx
@@ -55,21 +55,15 @@ const RoadmapPage: React.FC = () => {
                 Internet Computer?
               </h1>
               <p className="tw-lead-sm md:tw-lead mb-0">
-                The Internet Computer (IC) is a general-purpose blockchain-based
-                platform that can host and run virtually any kind of web-based
-                application in a decentralized manner. It is designed to provide
-                a World Computer that can replace traditional IT and enable a
-                new generation of Web3 services and applications that run
-                entirely on-chain. It can also play the role of Web3
-                orchestrator by interacting with traditional blockchains.
+              The Internet Computer (IC) is a general-purpose blockchain-based platform that can host and run web applications in a decentralized manner. 
+              It is designed to provide a World Computer that can replace traditional IT and enable a new generation of Web3 services and applications that run entirely on-chain. 
+              It can also play the role of Web3 orchestrator by interacting with other blockchains
               </p>
 
               <p className="tw-lead-sm md:tw-lead mb-0">
-                An open internet requires decentralization, which is solved
-                nicely by using blockchains. However, there are efficiency,
-                scalability, usability, and security issues with blockchain
-                protocols, a radical new design is needed to realize the vision
-                of a world computer.{" "}
+              An open internet requires decentralization, which can be achieved with blockchains. 
+              However, there are efficiency, scalability, usability, and security issues with blockchain protocols. 
+              A radical new design is needed to realize the vision of a world computer{" "}
               </p>
 
               <p className="tw-lead-sm md:tw-lead mb-0">
