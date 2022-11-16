52c080eb0b0341158fc0db7e60cca639bdf2a6d9
Artem <artem@dfinity.org>
2022-11-16 12:38:34 +0300

    Daemon typo

--- src/pages/features.tsx
+++ src/pages/features.tsx
@@ -669,7 +669,7 @@ function FeaturesPage() {
           </SmallCard>
           <SmallCard>
             <h4 className="tw-heading-6 md:tw-heading-5 mb-3">
-              Heartbeat (Deamon contracts)
+              Heartbeat (Daemon contracts)
             </h4>
             <p className="tw-paragraph-sm md:tw-paragraph text-black-60 mb-3">
             On traditional blockchain networks, smart contract computations can only be invoked by submitting a new transaction to their networks. The Internet Computer provides a means for canister smart contracts to be configured so that they are invoked by the blockchain itself, at some specified block interval.
