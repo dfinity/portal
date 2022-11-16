15579996acf191230cfdaa93414087590c88b8e8
Artem <artem@dfinity.org>
2022-11-16 13:42:55 +0300

    Added links

--- src/components/LandingPage/HeroSection/index.tsx
+++ src/components/LandingPage/HeroSection/index.tsx
@@ -23,12 +23,13 @@ export default function Hero(): JSX.Element {
             className="tw-lead-sm md:tw-lead mb-8"
             variants={transitions.item}
           >
-            The Internet Computer (ICP) is the first World Computer blockchain, powered by innovations 
-            in cryptography and computer science derived from deep crypto research begun in 2015. 
-            It is a limitless blockchain that runs with 20,000X more efficiency than the next best, 
+            The Internet Computer (ICP) is the first World Computer blockchain, 
+            powered by innovations in cryptography and computer science derived from <Link href="https://wiki.internetcomputer.org/wiki/History" className="link-subtle">deep crypto research begun in 2015</Link>. 
+            It is a limitless blockchain that runs with 20,000X more efficiency than the next best,
             and hosts smart contract code that directly serves web experiences to end users, interacts with web2, 
             and creates signed transactions that run on other blockchains. It supports end-to-end decentralization, 
-            in which traditional IT, such as Big Tech’s cloud services, can be completely replaced by blockchain smart contracts…
+            in which traditional IT, such as Big Tech’s cloud services, can be 
+            completely replaced by blockchain smart contracts…
           </AnimateSpawn>
           <AnimateSpawn
             el={motion.p}
