852998fc9510a8dbc817aa153d56d92f932b2c2a
Andrew Chepreghy <andris788@gmail.com>
2022-11-18 13:21:40 +0100

    Updated links

--- src/pages/sns.tsx
+++ src/pages/sns.tsx
@@ -132,7 +132,7 @@ function SnsPage() {
               </Link> */}
               <Link
                 className="button-outline"
-                href="/docs/current/tokenomics/sns/sns-intro-tokens"
+                href="/docs/current/developer-docs/integrations/sns"
               >
                 Learn about SNS DAOs
               </Link>
@@ -266,7 +266,7 @@ function SnsPage() {
             <MotionLink
               variants={transitions.item}
               className="button-outline-white"
-              href="/docs/current/developer-docs/integrations/sns/sns-intro#how-to-get-and-maintain-a-dao---different-options"
+              href="/docs/current/developer-docs/integrations/sns/get-sns/get-sns-intro/"
             >
               Learn about SNS DAOs
             </MotionLink>
