9e45d81d0eb6ef6529f3ea388d0e2fc606934ea2
Akos Olasz <olaszakos@gmail.com>
2022-12-02 12:42:46 +0100

    revert sw.ts to official version

--- service-worker/src/sw/sw.ts
+++ service-worker/src/sw/sw.ts
@@ -4,22 +4,14 @@ declare const self: ServiceWorkerGlobalScope;
 
 const DEBUG = true;
 
-console.log('Loaded IC service worker.');
-
 // Always install updated SW immediately
 self.addEventListener('install', (event) => {
-  console.log('Installed IC service worker.');
   event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', (event) => {
   // upon activation take control of all clients (tabs & windows)
   event.waitUntil(self.clients.claim());
-
-  console.log('updating sw...');
-  self.registration.update().then(() => {
-    console.log('done updating sw.');
-  });
 });
 
 // Intercept and proxy all fetch requests made by the browser or DOM on this scope.
