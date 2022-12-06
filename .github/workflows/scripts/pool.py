2900236840b7c2e183960415968d20299a9ca4ae
Akos Olasz <olaszakos@gmail.com>
2022-12-06 14:19:58 +0100

    add info about canister pool source code

--- .github/workflows/scripts/pool.py
+++ .github/workflows/scripts/pool.py
@@ -6,6 +6,11 @@ import sys
 import base64
 from ic.candid import encode, Types
 
+
+#
+# Interact with preview canister pool: https://github.com/dfinity/preview-canister-pool
+#
+
 private_key = base64.b64decode(os.environ["DFX_IDENTITY_PREVIEW"]).decode("utf-8")
 pool_id = os.environ["POOL_CANISTER_ID"]
 
