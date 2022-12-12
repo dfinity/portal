334718c6f177fa0ea0c353bffdb966454eda6ddb
rvanasa <ryan.vandersmith@dfinity.org>
2022-12-12 16:41:36 -0700

    Add MOPS to community development tools

--- src/components/Common/toolingItems.ts
+++ src/components/Common/toolingItems.ts
@@ -233,4 +233,10 @@ export const communityToolingItems = [
       "DevOps platform to build, ship and manage dapps faster on IC.",
     links: { external: "https://internetcomputerservices.com/" },
   },
+  {
+    title: "MOPS",
+    tags: ["CLI", "website", "development"],
+    description: "An on-chain package manager for Motoko.",
+    links: ["https://j4mwm-bqaaa-aaaam-qajbq-cai.ic0.app/"],
+  },
 ];
