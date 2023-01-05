4a79c5b1cf4e0a723b03f96debc65459588ea541
Akos Olasz <olaszakos@gmail.com>
2023-01-05 14:46:39 +0100

    add spawn animations; tweak styles

--- src/pages/internet-identity.tsx
+++ src/pages/internet-identity.tsx
@@ -2,13 +2,16 @@ import Link from "@docusaurus/Link";
 import { CardWithDescription } from "@site/src/components/Common/Card";
 import DarkHeroStyles from "@site/src/components/Common/DarkHeroStyles";
 import { resetNavBarStyle } from "@site/src/utils/reset-navbar-style";
+import transitions from "@site/static/transitions.json";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
+import { motion } from "framer-motion";
 import React, { useRef } from "react";
 import IconCryptography from "../../static/img/basics/icon-ii-cryptography.svg";
 import IconNoPassword from "../../static/img/basics/icon-ii-no-passwords.svg";
 import IconPrivacy from "../../static/img/basics/icon-ii-privacy.svg";
 import IconExternalLink from "../../static/img/external-link.svg";
+import AnimateSpawn from "../components/Common/AnimateSpawn";
 import ShareMeta from "../components/Common/ShareMeta";
 import { useDarkHeaderInHero } from "../utils/use-dark-header-in-hero";
 
@@ -20,16 +23,19 @@ const CardWithIcon: React.FC<{
   className?: string;
 }> = ({ children, title, icon, align = "left", className }) => {
   return (
-    <div
+    <motion.div
+      variants={transitions.item}
       className={clsx(
         "flex-1 flex flex-col text-black  border-white border-solid border bg-white-60 rounded-xl",
-        align === "center" ? "text-center py-12 px-6" : "p-6 md:px-8",
+        align === "center"
+          ? "text-center py-12 px-6 md:px-8"
+          : "p-6 md:px-8 md:py-12",
         className
       )}
     >
       <div
         className={clsx(
-          "mb-4 md:mb-10 flex",
+          "mb-4 md:mb-7 flex",
           align === "center" ? "justify-center" : ""
         )}
       >
@@ -37,7 +43,7 @@ const CardWithIcon: React.FC<{
       </div>
       <h3 className="tw-heading-5 md:tw-heading-3 mb-3">{title}</h3>
       {children}
-    </div>
+    </motion.div>
   );
 };
 
@@ -65,33 +71,51 @@ function InternetIdentityPage() {
           ref={heroRef}
         >
           {bgDark && <DarkHeroStyles bgColor="transparent"></DarkHeroStyles>}
-          <div className="container-10 pt-40 md:pb-36 md:pt-64 relative">
+          <AnimateSpawn
+            className="container-10 pt-40 md:pb-36 md:pt-64 relative"
+            variants={transitions.container}
+          >
             <div className="md:w-7/10">
-              <h1 className="tw-heading-3 md:tw-heading-2 mb-2 md:mb-6">
+              <motion.h1
+                className="tw-heading-3 md:tw-heading-2 mb-2 md:mb-6"
+                variants={transitions.item}
+              >
                 Identity on the Internet Computer
-              </h1>
-              <p className="tw-lead-sm md:tw-lead mb-0">
+              </motion.h1>
+              <motion.p
+                className="tw-lead-sm md:tw-lead mb-0"
+                variants={transitions.item}
+              >
                 A Web3 authentication framework providing a Web2 login
                 experience with blockchain security.
-              </p>
+              </motion.p>
             </div>
-            <div className="relative flex justify-center mt-8 md:absolute md:right-0 md:bottom-0">
+            <motion.div
+              className="relative flex justify-center mt-8 md:absolute md:right-0 md:bottom-0"
+              variants={transitions.container}
+            >
               <div className="blob left-[40%] -translate-x-1/2 -top-[270px] blob-white blob-md md:blob-xl md:-left-[700px] md:translate-x-0 md:-top-[600px]"></div>
               <img
                 src="/img/internet-identity/astronaut-4.webp"
                 alt=""
                 className="relative w-56 md:w-[417px]"
               />
-            </div>
-          </div>
+            </motion.div>
+          </AnimateSpawn>
         </section>
         <section className="container-12 relative mt-24 md:mt-40 mb-20">
-          <div className="md:w-8/12 md:mx-auto">
-            <h2 className="tw-heading-4 text-gradient mb-6 md:tw-heading-60 md:mb-8">
+          <AnimateSpawn
+            className="md:w-8/12 md:mx-auto"
+            variants={transitions.container}
+          >
+            <motion.h2
+              className="tw-heading-4 text-gradient mb-6 md:tw-heading-60 md:mb-8"
+              variants={transitions.item}
+            >
               Internet Identity is an anonymous gateway to applications on the
               Internet Computer
-            </h2>
-            <p className="mb-0">
+            </motion.h2>
+            <motion.p className="mb-0" variants={transitions.item}>
               <Link
                 className="link-primary link-with-icon"
                 href="https://internetcomputer.org/how-it-works#Web-access"
@@ -99,9 +123,12 @@ function InternetIdentityPage() {
                 <span>Learn how it works</span>
                 <IconExternalLink></IconExternalLink>
               </Link>
-            </p>
-          </div>
-          <div className="flex gap-3 relative flex-col md:flex-row mt-20 md:mt-40">
+            </motion.p>
+          </AnimateSpawn>
+          <AnimateSpawn
+            className="flex gap-3 relative flex-col md:flex-row mt-20 md:mt-40"
+            variants={transitions.container}
+          >
             <div className="hidden md:block blob blob-infinite blob-bottom-right blob-lg z-[-1]"></div>
             <CardWithIcon
               title="Biometric login"
@@ -135,44 +162,59 @@ function InternetIdentityPage() {
                 compatibility.
               </p>
             </CardWithIcon>
-          </div>
+          </AnimateSpawn>
 
-          <div className="md:mx-1/12 mt-24 md:mt-32 md:flex md:gap-1/10 md:items-center">
-            <div className="relative flex-[4] max-w-sm md:max-w-none">
+          <AnimateSpawn
+            className="md:mx-1/12 mt-24 md:mt-32 md:flex md:gap-1/10 md:items-center"
+            variants={transitions.container}
+          >
+            <motion.div
+              className="relative flex-[4] max-w-sm md:max-w-none"
+              variants={transitions.fadeIn}
+            >
               <div className="blob blob-purple blob-md blob-center-left z-[-1] opacity-70"></div>
               <img src="/img/internet-identity/astronaut-2.webp" alt="" />
-            </div>
+            </motion.div>
             <div className="mt-12 md:mt-0 flex-[5]">
-              <h2 className="tw-heading-4 mb-6 md:tw-heading-3">
-                Web3 security, Web2 convenience
-              </h2>
-              <p className="mb-3 tw-paragraph">
-                We've all grown accustomed to managing hundreds of usernames and
-                passwords, recovering forgotten accounts over text or email,
-                unwittingly permitting companies to profit from our data at the
-                cost of our personal privacy, all the while falling victim
-                threats of identity theft and fraud.
-              </p>
-              <p className="mb-0 tw-paragraph">
-                On the Internet Computer blockchain, users can securely
-                authenticate themselves without ever needing an email, username,
-                or password. Using any WebAuthn enabled device, users can login
-                conveniently without their information being monetized by tech
-                companies. Internet Identity makes it cryptographically
-                impossible for Web3 services to track your activity across
-                dapps.
-              </p>
-              <Link
-                className="link-primary link-with-icon mt-6"
-                href="https://identity.ic0.app/"
+              <motion.h2
+                className="tw-heading-4 mb-6 md:tw-heading-3"
+                variants={transitions.item}
               >
-                <span>Learn how to create one</span>
-                <IconExternalLink></IconExternalLink>
-              </Link>
+                Web3 security, Web2 convenience
+              </motion.h2>
+              <motion.div variants={transitions.item}>
+                <p className="mb-3 tw-paragraph">
+                  We've all grown accustomed to managing hundreds of usernames
+                  and passwords, recovering forgotten accounts over text or
+                  email, unwittingly permitting companies to profit from our
+                  data at the cost of our personal privacy, all the while
+                  falling victim threats of identity theft and fraud.
+                </p>
+                <p className="mb-0 tw-paragraph">
+                  On the Internet Computer blockchain, users can securely
+                  authenticate themselves without ever needing an email,
+                  username, or password. Using any WebAuthn enabled device,
+                  users can login conveniently without their information being
+                  monetized by tech companies. Internet Identity makes it
+                  cryptographically impossible for Web3 services to track your
+                  activity across dapps.
+                </p>
+                <Link
+                  className="link-primary link-with-icon mt-6"
+                  href="https://identity.ic0.app/"
+                >
+                  <span>Learn how to create one</span>
+                  <IconExternalLink></IconExternalLink>
+                </Link>
+              </motion.div>
             </div>
-          </div>
+          </AnimateSpawn>
         </section>
-        <section className="bg-infinite">
+        <AnimateSpawn
+          className="bg-infinite"
+          el={motion.section}
+          variants={transitions.container}
+        >
           <div className="container-10 pt-6 pb-20 md:pb-6 relative text-white flex flex-col items-start md:flex-row md:items-center md:gap-1/12">
             <div className="blob blob-white blob-md blob-center-right md:blob-bottom-right md:right-[200px] md:bottom-[-100px]"></div>
             <img
@@ -180,7 +222,10 @@ function InternetIdentityPage() {
               alt=""
               className="self-center md:order-2 md:flex-[4]"
             />
-            <div className="md:order-1 md:flex-[5]">
+            <motion.div
+              className="md:order-1 md:flex-[5]"
+              variants={transitions.item}
+            >
               <h2 className="tw-heading-4 md:tw-heading-3 mb-6 mt-6 md:mt-0">
                 Focused on <br />
                 privacy & security
@@ -191,27 +236,37 @@ function InternetIdentityPage() {
                 their data, connections are private, and access to Web3 services
                 at their fingertips.
               </p>
-            </div>
+            </motion.div>
           </div>
-        </section>
+        </AnimateSpawn>
 
         <section className="container-12 relative mb-20 md:mb-40 mt-20 md:mt-40">
-          <div className="mb-12 md:max-w-[680px] md:text-center md:mx-auto md:mb-8">
-            <h2 className="tw-heading-4 md:tw-heading-60 mb-6">
+          <AnimateSpawn
+            className="mb-12 md:max-w-[680px] md:text-center md:mx-auto md:mb-8"
+            variants={transitions.container}
+          >
+            <motion.h2
+              className="tw-heading-4 md:tw-heading-60 mb-6"
+              variants={transitions.item}
+            >
               Identity technologies on the Internet Computer
-            </h2>
-            <p className="tw-lead-sm mb-0">
+            </motion.h2>
+            <motion.p className="tw-lead-sm mb-0" variants={transitions.item}>
               Expand a dapp with Internet Identity or build your own
               authentication solution
-            </p>
-          </div>
-          <div className="flex gap-5 items-start flex-col md:flex-row">
+            </motion.p>
+          </AnimateSpawn>
+          <AnimateSpawn
+            className="flex gap-5 items-start flex-col md:flex-row"
+            variants={transitions.container}
+          >
             <CardWithIcon
               title="Internet Identity"
               icon={
                 <img
                   src="/img/internet-identity/astronaut-1.webp"
                   className="h-16"
+                  alt=""
                 ></img>
               }
               align="center"
@@ -290,22 +345,37 @@ function InternetIdentityPage() {
                 </Link>
               </p>
             </CardWithIcon>
-          </div>
+          </AnimateSpawn>
         </section>
 
         <section className="max-w-page relative mx-auto mb-20 px-6 md:mb-40 md:px-15">
-          <div className=" relative text-white">
-            <div className="blob blob-purple blob-md blob-center z-[-1] md:blob-xl"></div>
-            <h2 className="tw-heading-5 text-center mb-2 w-full mx-auto md:tw-heading-60 md:mb-6 lg:w-8/12">
+          <AnimateSpawn
+            className=" relative text-white"
+            variants={transitions.container}
+          >
+            <motion.div
+              className="blob blob-purple blob-md blob-center z-[-1] md:blob-xl"
+              variants={transitions.fadeIn}
+            ></motion.div>
+            <motion.h2
+              className="tw-heading-5 text-center mb-2 w-full mx-auto md:tw-heading-60 md:mb-6 lg:w-8/12"
+              variants={transitions.item}
+            >
               Build with Identity
-            </h2>
-            <p className="tw-lead-sm mb-0 text-center mx-auto md:w-6/12">
+            </motion.h2>
+            <motion.p
+              className="tw-lead-sm mb-0 text-center mx-auto md:w-6/12"
+              variants={transitions.item}
+            >
               Learn how to build a user-friendly identity products on the
               Internet Computer using our core identity component, Internet
               Identity.
-            </p>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 mt-8 md:mt-16">
+            </motion.p>
+          </AnimateSpawn>
+          <AnimateSpawn
+            className="grid grid-cols-1 sm:grid-cols-2 gap-2 mt-8 md:mt-16"
+            variants={transitions.container}
+          >
             <CardWithDescription
               title="Dev Toolkits"
               description="See the docs, get building."
@@ -326,7 +396,7 @@ function InternetIdentityPage() {
               description="Internet Identity: Easy Web3 Authentication."
               href="https://medium.com/dfinity/internet-identity-the-end-of-usernames-and-passwords-ff45e4861bf7"
             />
-          </div>
+          </AnimateSpawn>
         </section>
       </main>
     </Layout>
