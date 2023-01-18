5fc15c792c3b66c25cb3ba82709f31931018505d
Akos Olasz <olaszakos@gmail.com>
2023-01-18 13:44:09 +0100

    png->webp; fix style error

--- src/components/DocsHome/Contribute/index.tsx
+++ src/components/DocsHome/Contribute/index.tsx
@@ -5,7 +5,7 @@ import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import transitions from "@site/static/transitions.json";
 import RightArrowSVG from "@site/static/img/svgIcons/rightArrowIcon.svg";
-import contribute from "@site/static/img/developers/contribute.png";
+import contribute from "@site/static/img/developers/contribute.webp";
 import AnimateSpawn from "../../Common/AnimateSpawn";
 
 const cardsContent = [
--- src/components/LandingPage/StartBuilding/index.tsx
+++ src/components/LandingPage/StartBuilding/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import styles from "@site/src/components/LandingPage/StartBuilding/index.module.css";
-import motokoBG from "@site/static/img/motokoPlayground.png";
+import motokoBG from "@site/static/img/motoko.webp";
 import RightArrowSVG from "@site/static/img/svgIcons/rightArrowIcon.svg";
 import BackgroundGradient from "@site/static/img/startBuilding/bgGradient.png";
 import GamingIcon from "@site/static/img/startBuilding/gaming.svg";
