bc5219581df998708d081d2cfc22e670508c3f95
Akos Olasz <olaszakos@gmail.com>
2023-01-30 17:46:54 +0100

    update meta

--- src/pages/bitcoin-integration.tsx
+++ src/pages/bitcoin-integration.tsx
@@ -8,6 +8,7 @@ import Layout from "@theme/Layout";
 import React, { useRef } from "react";
 import Content from "../components/BitcoinIntegrationPage/Content";
 import DarkHeroStyles from "../components/Common/DarkHeroStyles";
+import ShareMeta from "../components/Common/ShareMeta";
 import { useDarkHeaderInHero } from "../utils/use-dark-header-in-hero";
 
 function BitcoinIntegration() {
@@ -17,25 +18,14 @@ function BitcoinIntegration() {
 
   return (
     <Layout
-      title="Bitcoin Integration"
-      description="The Internet Computer enables direct integration with the Bitcoin network. By way of bridge-less communication with the Bitcoin network and a novel threshold ECDSA protocol, canisters on the Internet Computer can now securely receive, hold, and send bitcoins."
+      title="Bitcoin on ICP"
+      description="The Internet Computer (ICP) cryptographically integrates with the
+      Bitcoin network, unleashing a plethora of opportunities to securely
+      execute bitcoin transactions 100% on chain."
       editPath={`https://github.com/dfinity/portal/edit/master/${__filename}`}
     >
-      <Head>
-        <meta
-          property="og:image"
-          content={
-            "https://internetcomputer.org/img/shareImages/share-bitcoin-integration.jpeg"
-          }
-        />
-        <meta
-          name="twitter:image"
-          content={
-            "https://internetcomputer.org/img/shareImages/share-bitcoin-integration.jpeg"
-          }
-        />
-        <title>Bitcoin Integration</title>
-      </Head>
+      <ShareMeta image="/img/shareImages/share-bitcoin-integration.jpeg"></ShareMeta>
+
       <main
         className="text-black relative overflow-hidden"
         style={{
