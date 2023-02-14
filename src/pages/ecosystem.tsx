d6a7d313118b83e0a60b283e916ff7e35b8f5da9
Akos Olasz <olaszakos@gmail.com>
2023-02-14 16:24:57 +0100

    loading indicator for showcase videos/images

--- src/pages/ecosystem.tsx
+++ src/pages/ecosystem.tsx
@@ -103,8 +103,9 @@ const LargeProjectMedia: React.FC<{
   }, [shown, inView]);
 
   return (
-    <div ref={ref} className="flex min-h-full">
-      {shown && (
+    <div ref={ref} className="flex min-h-full relative">
+      <div className="z-[-1] animate-pulse absolute inset-0 bg-black mr-px"></div>
+      {shown ? (
         <video
           loop
           muted
@@ -114,6 +115,8 @@ const LargeProjectMedia: React.FC<{
         >
           <source src={project.video} type={project.videoContentType} />
         </video>
+      ) : (
+        <div className="aspect-video w-full"></div>
       )}
     </div>
   );
@@ -212,7 +215,8 @@ const LargeCard = ({ project }: { project: ShowcaseProject }) => {
         {project.video ? (
           <LargeProjectMedia project={project}></LargeProjectMedia>
         ) : (
-          <div className="flex w-full h-full">
+          <div className="flex w-full h-full relative">
+            <div className="z-[-1] animate-pulse absolute inset-0 bg-black mr-px"></div>
             <img
               loading="lazy"
               alt=""
