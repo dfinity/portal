2b54162ea6eb89a625c05d26ae6b97b0f8080cb6
Nathan Mc Grath <contact@nathanos.dev>
2023-03-07 18:37:49 +0100

    Update sidebars.js
    Co-authored-by: r-birkner <103420898+r-birkner@users.noreply.github.com>

--- sidebars.js
+++ sidebars.js
@@ -24,7 +24,7 @@ const sidebars = {
       items: [
         "references/ic-interface-spec",
         "references/id-encoding-spec",
-        "reference/http-gateway-protocol-spec",
+        "references/http-gateway-protocol-spec",
       ]
     },
     {
