bae10e168c39a661f72aa7ecad1947f13c0da637
harpa12 <102589890+harpa12@users.noreply.github.com>
2023-03-07 18:26:23 +0100

    Update index.tsx

--- src/components/BitcoinIntegrationPage/Content/index.tsx
+++ src/components/BitcoinIntegrationPage/Content/index.tsx
@@ -101,7 +101,7 @@ const Content: React.FC = () => {
           <p className="tw-lead-sm md:tw-lead mb-0">
             Chain-Key Bitcoin (ckBTC) acts as a bitcoin ‘twin’ on ICP. It is
             issued and redeemed via canister smart contracts controlled by the
-            NNS and cryptographically backed 1:1 with real bitcoin. It’s fast,
+            NNS and cryptographically secured 1:1 with real bitcoin. It’s fast,
             low-tx-fee bitcoin on ICP with no intermediary.
           </p>
         </div>
@@ -137,16 +137,11 @@ const Content: React.FC = () => {
             using bitcoin practical for everyday transactions.
           </p>
           <p className="tw-paragraph mb-3">
-            The ckBTC token eliminates the need for intermediaries as it is
-            issued and redeemed through canister smart contracts built upon the
-            native Bitcoin integration. When the ckBTC canister receives real
-            bitcoin, it issues ckBTC backed 1:1 by that bitcoin. The data served
-            by the ckBTC canister is transparent, so anyone can verify if their
-            ckBTC is backed 1:1 with bitcoin. Users can also redeem their ckBTC
-            for real Bitcoin at any time.
+            End-users can seemlessly transfer bitcoin
+            any time between blockchains without any third-party bridges or custodians.
           </p>
           <p className="tw-paragraph mb-3">
-            The ckBTC token is compliant with ICRC-1, the fungible token
+            ckBTC is compliant with ICRC-1, the fungible token
             standard on ICP, and can be integrated easily by any service, e.g.,
             wallets or DEXs.
           </p>
@@ -193,11 +188,11 @@ const Content: React.FC = () => {
             compromise.
           </p>
           <p className="tw-paragraph mb-8">
-            ckBTC is issued and redeemed via a canister smart contract backed 1:1
-            with real bitcoin — no bridge or custodian, and verifiable by
-            anyone. The ckBTC canister is controlled by the NNS DAO, which
-            means no single entity can initiate malicious activities without
-            the approval of ICP governance token holders.
+            ckBTC is issued and redeemed via a canister smart contract — 
+            no bridge or custodian, and verifiable byanyone. The ckBTC 
+            canister is controlled by the NNS DAO, whichmeans no single 
+            entity can initiate malicious activities without the approval 
+            of ICP governance token holders.
           </p>
           <p className="tw-paragraph mb-0 font-bold">Not a Lightning Network</p>
           <p className="tw-paragraph mb-3">
