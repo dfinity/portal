2accdea76ba01cd6d1c749e7170e78280ca70049
Akos Olasz <olaszakos@gmail.com>
2023-05-22 13:35:22 +0200

    report url params to matomo

--- plugins/matomo/module.js
+++ plugins/matomo/module.js
@@ -3,7 +3,7 @@ const module = {
   onRouteUpdate({ location }) {
     // wait for page title to get updated
     setTimeout(() => {
-      window._paq.push(["setCustomUrl", location.pathname]);
+      window._paq.push(["setCustomUrl", location.pathname + location.search]);
       window._paq.push(["setDocumentTitle", document.title]);
       window._paq.push(["trackPageView"]);
     });
