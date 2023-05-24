62ed78c7c9f2b01a0c78a47589264309737ac8c8
Andrew <andris788@gmail.com>
2023-05-24 11:53:08 +0200

    Changed BTC integration page

--- src/components/BitcoinIntegrationPage/Content/index.tsx
+++ src/components/BitcoinIntegrationPage/Content/index.tsx
@@ -104,8 +104,6 @@ const Content: React.FC = () => {
             — a multi-chain bitcoin twin, trustlessly created by chain-key cryptography and a
             pair of canister smart contracts that directly hold raw bitcoin without relying
             on intermediaries.
-
-
           </p>
         </div>
         <div className="md:w-8/10 md:mx-auto flex my-6 md:my-8">
@@ -136,29 +134,27 @@ const Content: React.FC = () => {
             Recent surge in popularity of Bitcoin Ordinals and BRC-20 tokens
             resulted in Bitcoin's network to be highly congested. This pushed
             transaction fees above $30, rendering casual every-day transactions
-            completely impractical. Chain-key Bitcoin (ckBTC), a multi-chain bitcoin
+            completely impractical.
+          </p>
+          <p className="tw-paragraph mb-3">
+            Chain-key Bitcoin (ckBTC), a multi-chain bitcoin
             twin on ICP introduces layer 2 functionality fuelled by ICP
             properties like speed, scalability and low transaction fees to
             bitcoin. ckBTC helps reduce the load on the Bitcoin network, while
-            making everyday bitcoin transactions feasible. Allowing bitcoin to
-            be used for every-day transactions realizes a key part of Satoshi's original
-            vision.
-          </p>
-          <p className="tw-paragraph mb-3">
-            End-users can seemlessly transfer bitcoin
-            any time between blockchains without any third-party bridges or custodians.
+            making every-day bitcoin transactions feasible, which realizes a key
+            part of Satoshi's original vision.
           </p>
           <p className="tw-paragraph mb-3">
             ckBTC implements ICRC-1, the fungible token standard on the Internet
-            Computer, and can be integrated easily by any service, e.g.,
-            wallets or DEXs.
+            Computer, and can be integrated easily by any Web3 service running on
+            ICP.
           </p>
           <p className="mb-0 mt-8 flex gap-6 flex-col md:flex-row items-start md:items-center">
             <Link
               className="button-outline"
-              href="https://dashboard.internetcomputer.org/bitcoin"
+              href="/ckbtc"
             >
-              ckBTC on the dashboard
+              More on ckBTC
             </Link>
             <Link
               href="https://internetcomputer.org/docs/current/developer-docs/integrations/bitcoin/ckbtc"
@@ -169,66 +165,6 @@ const Content: React.FC = () => {
             </Link>
           </p>
         </div>
-      </AnimateSpawn>
-      <AnimateSpawn
-        className="container-10 flex flex-col md:flex-row gap-8 md:gap-1/10 mb-20 md:mb-40"
-        variants={transitions.container}
-        el={motion.section}
-      >
-        <motion.div className="flex-[5] md:order-2" variants={transitions.item}>
-          <h2 className="tw-heading-5 md:tw-heading-3 mb-8 md:mb-3">
-            What ckBTC is <span className="text-razzmatazz">NOT</span>
-          </h2>
-          <p className="mb-8 md:hidden max-w-md mx-auto md:max-w-none">
-            <img
-              src="/img/bitcoin-integration/ckBTC-img-2.webp"
-              alt=""
-              className="w-full "
-            />
-          </p>
-          <p className="tw-paragraph mb-0 font-bold">Not a bridged token</p>
-          <p className="tw-paragraph mb-3">
-            Most blockchains rely on so-called bridges to wrap and unwrap
-            tokens, such as bitcoin. The recent incident where the FTX exchange
-            acted as the custodian, and Sollet the bridge for wrapping and
-            unwrapping BTC and ETH on Solana, demonstrated how bridges and
-            intermediaries are highly vulnerable to hacks or other forms of
-            compromise.
-          </p>
-          <p className="tw-paragraph mb-8">
-            ckBTC is issued and redeemed via an open source canister smart
-            contract — no bridge or custodian involved, verifiable by anyone.
-            The ckBTC canisters are controlled by the NNS DAO, which means no single 
-            entity can initiate malicious activities without the approval 
-            of ICP governance token holders.
-          </p>
-          <p className="tw-paragraph mb-0 font-bold">Not Lightning Network</p>
-          <p className="tw-paragraph mb-3">
-            While a lightning network has many advantages, such as faster
-            transactions and lower fees, transactions go through peer-to-peer
-            payment channels that run on centralized cloud systems. ckBTC
-            brings layer 2 functionality to the Bitcoin network — fast,
-            low-fee payments — without the risk or limitations of a lightning network in that all
-            transactions are held on the ckBTC ledger and never go through
-            centralized channels.
-          </p>
-
-          <p className="tw-paragraph mb-0">
-            True decentralized transactions on the Internet Computer are made
-            possible via inter-chain communication with the Bitcoin network and
-            chain-key ECDSA signatures.
-          </p>
-        </motion.div>
-        <motion.div
-          className="flex-[4] md:order-1 items-start hidden md:flex"
-          variants={transitions.item}
-        >
-          <img
-            src="/img/bitcoin-integration/ckBTC-img-2.webp"
-            alt=""
-            className="w-full max-w-md mx-auto md:max-w-none"
-          />
-        </motion.div>
       </AnimateSpawn>
 
       <AnimateSpawn className="mb-20 md:mb-40" variants={transitions.container}>
@@ -327,6 +263,16 @@ const Content: React.FC = () => {
                 own governance token, CHAT.
               </p>
             </div>
+          </Link>
+        </div>
+        <div className="pt-20 text-center flex flex-col items-center gap-8 relative">
+          <div className="md:hidden blob blob-purple blob-center blob-md z-[-1]"></div>
+
+          <Link
+            className="button-primary"
+            href="/ecosystem?source=nav&tag=Bitcoin"
+          >
+            More projects using bitcoin
           </Link>
         </div>
       </AnimateSpawn>
