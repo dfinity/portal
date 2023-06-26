b866867779ff24771ebc14a3997781afe4892ad4
Diego Prats <diegoprats@Diego-Prats-MacBook-Pro.local>
2023-06-26 10:04:06 -0700

    update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -396,10 +396,9 @@ const sidebars = {
                       type: "category",
                       label: "Introduction to the SNS",
                       items: [
-                        "developer-docs/integrations/sns/lifecycle-sns/sns-intro-highlevel",
-                        "developer-docs/integrations/sns/lifecycle-sns/sns-architecture",
-                        "developer-docs/integrations/sns/lifecycle-sns/sns-launch",
-                        "developer-docs/integrations/sns/lifecycle-sns/dao-alternatives",
+                        "developer-docs/integrations/sns/introduction/sns-intro-high-level",
+                        "developer-docs/integrations/sns/introduction/sns-architecture",
+                        "developer-docs/integrations/sns/introduction/dao-alternatives",
                       ],
                     },
                     {
@@ -422,11 +421,11 @@ const sidebars = {
                       label: "Integrating with an SNS",
                       link: {
                         type: "doc",
-                        id: "developer-docs/integrations/sns/integrate-sns/index",
+                        id: "developer-docs/integrations/sns/integrating/index",
                     },
                       items: [
-                        "developer-docs/integrations/sns/integrate-sns/index-integration",
-                        "developer-docs/integrations/sns/integrate-sns/ledger-integration",
+                        "developer-docs/integrations/sns/integrating/index-integration",
+                        "developer-docs/integrations/sns/integrating/ledger-integration",
                       ],
                     },
                     {
@@ -434,29 +433,35 @@ const sidebars = {
                       label: "Testing an SNS",
                       link: {
                         type: "doc",
-                        id: "developer-docs/integrations/sns/get-sns/get-sns-intro",
-                    },
+                        id: "developer-docs/integrations/sns/testing/get-sns-intro",
+                      },
                       items: [
-                        "developer-docs/integrations/sns/get-sns/local-testing",
-                        "developer-docs/integrations/sns/get-sns/testflight",
+                        "developer-docs/integrations/sns/testing/local-testing",
+                        "developer-docs/integrations/sns/testing/testflight",
                       ],
                     },
                     {
                       type: "category",
                       label: "Launching an SNS",
-                      type: "doc",
-                      id: "developer-docs/integrations/sns/launch-sns/launch-sns",
+                      link: {
+                        type: "doc",
+                        id: "developer-docs/integrations/sns/launching/launch-summary",
+                      },
+                      items: [
+                        "developer-docs/integrations/sns/launching/launch-summary",
+                        "developer-docs/integrations/sns/launching/launch-steps",
+                      ],
                     },
                     {
                       type: "category",
                       label: "Managing an SNS",
                       link: {
                         type: "doc",
-                        id: "developer-docs/integrations/sns/managing-sns/manage-sns-intro",
+                        id: "developer-docs/integrations/sns/managing/manage-sns-intro",
                     },
                       items: [
-                        "developer-docs/integrations/sns/managing-sns/sns-governance",
-                        "developer-docs/integrations/sns/managing-sns/cycles-usage",
+                        "developer-docs/integrations/sns/managing/sns-governance",
+                        "developer-docs/integrations/sns/managing/cycles-usage",
                       ],
                     },
                   ],
