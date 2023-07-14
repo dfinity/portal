e51c894131288fe2e3c698a37c79f329e879a129
Andrew Chepreghy <andris788@gmail.com>
2023-07-14 16:56:26 +0200

    Update Decks.tsx

--- src/components/LandingPage/Decks/Decks.tsx
+++ src/components/LandingPage/Decks/Decks.tsx
@@ -85,7 +85,7 @@ const Decks = () => {
       >
         <CardLabel>Download Presentation</CardLabel>
         <CardTitle>Internet Computer</CardTitle>
-        <CardBody>PDF - Last update: June 20th, 2023</CardBody>
+        <CardBody>PDF - Last update: July 14th, 2023</CardBody>
         <CardImage
           src="/img/home/deck-astronaut.webp"
           alt="ICP Pitch Presenation"
