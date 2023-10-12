fc8345ba1760dd15185ec62c0907228c5badc11a
Jason I <jason.ibrahim@dfinity.org>
2023-10-12 10:28:07 -0700

    chore: add CSP to docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -404,6 +404,17 @@ const config = {
   customFields: {
     searchCanisterId: "5qden-jqaaa-aaaam-abfpa-cai",
   },
+  scripts: [
+    {
+      src: "https://widget.kapa.ai/kapa-widget.bundle.js",
+      "data-website-id": "08910249-851f-465b-b60f-238d84e1afc1",
+      "data-project-name": "Internet Computer",
+      "data-project-color": "#172234",
+      "data-project-logo":
+        "https://s3.coinmarketcap.com/static-gravity/image/2fb1bc84c1494178beef0822179d137d.png",
+      async: true,
+    },
+  ],
   plugins: [
     ["docusaurus2-dotenv", { systemvars: true }],
     "docusaurus-plugin-sass",
--- src/theme/Root.js
+++ src/theme/Root.js
@@ -7,23 +7,23 @@ export default function Root({ children }) {
 
   return (
     <>
-      {isDocsPage && (
-        // AI chatbot integration via Kapa
-        <Head>
-          <meta
-            http-equiv="Content-Security-Policy"
-            content="default-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.matomo.cloud https://widget.kapa.ai https://www.google.com https://www.gstatic.com"
-          />
-          <script
-            defer
-            src="https://widget.kapa.ai/kapa-widget.bundle.js"
-            data-website-id="08910249-851f-465b-b60f-238d84e1afc1"
-            data-project-name="Internet Computer"
-            data-project-color="#172234"
-            data-project-logo="https://s3.coinmarketcap.com/static-gravity/image/2fb1bc84c1494178beef0822179d137d.png"
-          />
-        </Head>
-      )}
+      {/*{isDocsPage && (*/}
+      {/*  // AI chatbot integration via Kapa*/}
+      {/*  <Head>*/}
+      {/*    <meta*/}
+      {/*      http-equiv="Content-Security-Policy"*/}
+      {/*      content="default-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.matomo.cloud https://widget.kapa.ai https://www.google.com https://www.gstatic.com"*/}
+      {/*    />*/}
+      {/*    <script*/}
+      {/*      defer*/}
+      {/*      src="https://widget.kapa.ai/kapa-widget.bundle.js"*/}
+      {/*      data-website-id="08910249-851f-465b-b60f-238d84e1afc1"*/}
+      {/*      data-project-name="Internet Computer"*/}
+      {/*      data-project-color="#172234"*/}
+      {/*      data-project-logo="https://s3.coinmarketcap.com/static-gravity/image/2fb1bc84c1494178beef0822179d137d.png"*/}
+      {/*    />*/}
+      {/*  </Head>*/}
+      {/*)}*/}
       {children}
     </>
   );
