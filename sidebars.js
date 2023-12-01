685b04028a6d0d75caa56ee24dbc5bae10164f94
Jessie Mongeon <133128541+jessiemongeon1@users.noreply.github.com>
2023-12-01 09:26:26 -0600

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -176,7 +176,7 @@ const sidebars = {
       items: [
         {
           type: "category",
-          label: "How the ICP works",
+          label: "How the Internet Computer Protocol works",
           link: {
             type: "doc",
             id: "concepts/index",
