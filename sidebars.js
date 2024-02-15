84b5a5ff7e6994ae86a4f8f79e0c32818452ebc3
Dimitris Sarlis <dimitrios.sarlis@dfinity.org>
2024-02-15 16:10:44 +0000

    Add page to sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -274,6 +274,10 @@ const sidebars = {
         {
           type: "category",
           label: "Call",
+          link: {
+            type: "doc",
+            id: "developer-docs/smart-contracts/call-overview",
+	  },
           items: [
             {
               type: "category",
