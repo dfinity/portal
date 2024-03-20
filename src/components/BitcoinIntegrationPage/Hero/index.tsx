522934fabb02bb48757ea1bda8afa6d2d2bde417
Diego Prats <diego.prats@gmail.com>
2024-03-20 08:03:37 -0700

    Update src/components/BitcoinIntegrationPage/Hero/index.tsx
    Co-authored-by: Björn Tackmann <54846571+Dfinity-Bjoern@users.noreply.github.com>

--- src/components/BitcoinIntegrationPage/Hero/index.tsx
+++ src/components/BitcoinIntegrationPage/Hero/index.tsx
@@ -24,7 +24,7 @@ const Hero = forwardRef<HTMLDivElement, {}>((_, ref) => {
           >
             The Internet Computer (ICP) cryptographically integrates with the
             Bitcoin network, enabling IC smart contracts to call into the Bitcoin network. This creates native bitcoin smart contract
-            use cases — all without the need for bridges and backed by Chain Key cryptography.
+            use cases — all without the need for bridges and backed by chain-key cryptography.
           </motion.p>
         </AnimateSpawn>
       </div>
