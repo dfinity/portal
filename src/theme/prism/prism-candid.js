719fef16df5835989e58d6fdefb2d923adf735b7
rvanasa <ryan.vandersmith@dfinity.org>
2024-03-21 14:35:27 -0600

    Remove 'highlight.js' dependency and adjust grammars

--- src/theme/prism/prism-candid.js
+++ src/theme/prism/prism-candid.js
@@ -12,14 +12,12 @@ Prism.languages.candid = {
     }
   ],
   keyword:
-    /\b(?:opt|vec|record|variant|func|blob|principal|nat|nat8|nat16|nat32|nat64|int|int8|int16|int32|int64|float32|float64|bool|text|null|reserved|empty|oneway|query')\b/,
+    /\b(?:opt|vec|record|variant|func|blob|principal|(?:int|nat)(?:|8|16|32|64)|float(?:32|64)|bool|text|null|reserved|empty|oneway|query')\b/,
   boolean: /\b(?:_|false|true)\b/,
   number: [
     /[+-]?\\b0[xX]([A-Fa-f0-9_]+)/,
     /[+-]?\\b(\\d[\\d_]*(\\.[0-9_]+)?([eE][+-]?[0-9_]+)?)/
   ],
-  operator:
-    /[*\/%^!=]=?|\+[=+]?|-[=-]?|\|[=|]?|&(?:=|&|\^=?)?|>(?:>=?|=)?|<(?:<=?|=|-)?|:=|\.\.\./,
   builtin: /\b(?:import|service|type)\b/
 };
 
--- src/theme/prism/prism-motoko.js
+++ src/theme/prism/prism-motoko.js
@@ -1,98 +1,27 @@
-const keywords = [
-  "actor",
-  "and",
-  "async",
-  "assert",
-  "await",
-  "break",
-  "case",
-  "catch",
-  "class",
-  "continue",
-  "debug",
-  "debug_show",
-  "do",
-  "else",
-  "false",
-  "flexible",
-  "for",
-  "from_candid",
-  "func",
-  "if",
-  "ignore",
-  "in",
-  "import",
-  "label",
-  "let",
-  "loop",
-  "module",
-  "not",
-  "null",
-  "object",
-  "or",
-  "private",
-  "public",
-  "query",
-  "return",
-  "shared",
-  "stable",
-  "switch",
-  "system",
-  "throw",
-  "to_candid",
-  "true",
-  "try",
-  "type",
-  "var",
-  "while",
-  "with"
-];
-
-const typeKeywords = [
-  "Any",
-  "None",
-  "Null",
-  "Bool",
-  "Int",
-  "Int8",
-  "Int16",
-  "Int32",
-  "Int64",
-  "Nat",
-  "Nat8",
-  "Nat16",
-  "Nat32",
-  "Nat64",
-  "Float",
-  "Char",
-  "Text",
-  "Blob",
-  "Error",
-  "Principal"
-];
+import { keywords, typeKeywords } from "motoko/lib/keywords";
 
 Prism.languages.motoko = {
   string: [
     {
       pattern: /r(#*)"(.|\n)*?"\1(?!#)/,
       lookbehind: true,
-      greedy: true
+      greedy: true,
     },
     {
       pattern: /b?'\\?(x\w{2}|u\w{4}|U\w{8}|.)'/,
       lookbehind: true,
-      greedy: true
-    }
+      greedy: true,
+    },
   ],
-  keyword: new RegExp(keywords.filter((k) => k !== "async").join("|")),
+  keyword: new RegExp(
+    `\\b(?:${keywords.filter((k) => k !== "async").join("|")})\\b`
+  ),
   boolean: /\b(?:_|true|false|null)\b/,
   number: [
     /[+-]?\\b0[xX]([A-Fa-f0-9_]+)/,
-    /[+-]?\\b(\\d[\\d_]*(\\.[0-9_]+)?([eE][+-]?[0-9_]+)?)/
+    /[+-]?\\b(\\d[\\d_]*(\\.[0-9_]+)?([eE][+-]?[0-9_]+)?)/,
   ],
-  operator:
-    /[*\/%^!=]=?|\+[=+]?|-[=-]?|\|[=|]?|&(?:=|&|\^=?)?|>(?:>=?|=)?|<(?:<=?|=|-)?|:=|\.\.\./,
-  builtin: new RegExp(`async ${typeKeywords.join("|")}`)
+  builtin: /\b(?:[A-Z][a-z0-9_]*)\b/
 };
 
 Prism.languages.mo = Prism.languages.motoko;
