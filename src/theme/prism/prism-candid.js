fb5e64a80c879f4f3efd8e91b0ff0d35662e9a9e
Jason I <jason.ibrahim@dfinity.org>
2024-03-21 13:28:49 -0700

    chore: update grammar

--- src/theme/prism/prism-candid.js
+++ src/theme/prism/prism-candid.js
@@ -1,26 +1,14 @@
-Prism.languages.candid = {
-  string: [
-    {
-      pattern: /r(#*)"(.|\n)*?"\1(?!#)/,
-      lookbehind: true,
-      greedy: true
-    },
-    {
-      pattern: /b?'\\?(x\w{2}|u\w{4}|U\w{8}|.)'/,
-      lookbehind: true,
-      greedy: true
-    }
-  ],
+Prism.languages.candid = Prism.languages.extend("clike", {
   keyword:
     /\b(?:opt|vec|record|variant|func|blob|principal|nat|nat8|nat16|nat32|nat64|int|int8|int16|int32|int64|float32|float64|bool|text|null|reserved|empty|oneway|query|composite_query')\b/,
   boolean: /\b(?:_|false|true)\b/,
   number: [
     /[+-]?\\b0[xX]([A-Fa-f0-9_]+)/,
-    /[+-]?\\b(\\d[\\d_]*(\\.[0-9_]+)?([eE][+-]?[0-9_]+)?)/
+    /[+-]?\\b(\\d[\\d_]*(\\.[0-9_]+)?([eE][+-]?[0-9_]+)?)/,
   ],
   operator:
     /[*\/%^!=]=?|\+[=+]?|-[=-]?|\|[=|]?|&(?:=|&|\^=?)?|>(?:>=?|=)?|<(?:<=?|=|-)?|:=|\.\.\./,
-  builtin: /\b(?:import|service|type)\b/
-};
+  builtin: /\b(?:import|service|type)\b/,
+});
 
 Prism.languages.did = Prism.languages.candid;
