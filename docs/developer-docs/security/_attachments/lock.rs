55c504cf37f82bdd13a0c3070d20aac217dd34ba
Martin Raszyk <martin.raszyk@dfinity.org>
2024-04-10 11:06:24 +0200

    feat: add canister locking example with generic types to security best practices

---
+++ docs/developer-docs/security/_attachments/lock.rs
@@ -0,0 +1,40 @@
+use std::cell::RefCell;
+use std::cmp::Ord;
+use std::collections::BTreeSet;
+use std::rc::Rc;
+
+pub struct State<T: Ord> {
+    pending_requests: BTreeSet<T>,
+}
+
+impl<T: Ord> Default for State<T> {
+    fn default() -> Self {
+        Self {
+            pending_requests: BTreeSet::new(),
+        }
+    }
+}
+
+pub struct CallerGuard<T: Ord> {
+    state: Rc<RefCell<State<T>>>,
+    lock: T,
+}
+
+impl<T: Clone + Ord> CallerGuard<T> {
+    pub fn new(state: Rc<RefCell<State<T>>>, lock: T) -> Option<Self> {
+        {
+            let pending_requests = &mut state.borrow_mut().pending_requests;
+            if pending_requests.contains(&lock) {
+                return None;
+            }
+            pending_requests.insert(lock.clone());
+        }
+        Some(Self { state, lock })
+    }
+}
+
+impl<T: Ord> Drop for CallerGuard<T> {
+    fn drop(&mut self) {
+        self.state.borrow_mut().pending_requests.remove(&self.lock);
+    }
+}
