95e50ebff459082122a6d05a861a5e413bf9c425
reigj1 <rei.gjyli@Rei-Gjylis-Mac.local>
2024-04-25 12:01:59 +0200

    ignore broken links for preview build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -486,7 +486,7 @@ const config = {
     ? `https://${process.env.PREVIEW_CANISTER_ID}.icp0.io`
     : "https://internetcomputer.org",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "throw",
   favicon: "img/favicon-32x32.png",
   organizationName: "dfinity",
