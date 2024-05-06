2a83990c0c26d347f7163d2378818e930226ede8
David Aerne <meodai@gmail.com>
2024-05-06 11:53:50 +0200

    overlay

--- src/pages/roadmap.tsx
+++ src/pages/roadmap.tsx
@@ -11,7 +11,7 @@ import AnimateSpawn from "../components/Common/AnimateSpawn";
 import DarkHeroStyles from "../components/Common/DarkHeroStyles";
 import ShareMeta from "../components/Common/ShareMeta";
 import DomainCard from "../components/RoadmapPage/DomainCard";
-import Overlay from "../components/RoadmapPage/Overlay";
+i//import Overlay from "../components/RoadmapPage/Overlay";
 import { RootObject } from "../components/RoadmapPage/RoadmapTypes";
 import { FormDescription } from "@site/docs/references/samples/motoko/ic-pos/src/icpos_frontend/components/ui/form";
 import { theme } from "@site/tailwind.config";
@@ -20,22 +20,24 @@ const MotionLink = motion(Link);
 
 const data = roadmapData as RootObject[];
 
+function elementCount (milestoneElements: any[], progress: string) {
+  return milestoneElements.filter(
+    (element) => element.progress === progress
+  ).length;
+}
+
 const milestoneElementsToProgress = (milestoneElements: any[]) => {
   const elementsCount = milestoneElements.length;
-  const elementsCountInProgress = milestoneElements.filter(
-    (element) => element.progress === "in_progress"
-  ).length;
-  const elementsCountDone = milestoneElements.filter(
-    (element) => element.progress === "deployed"
-  ).length;
+  const elementsCountInProgress = elementCount(milestoneElements, "in_progress")
+  const elementsCountDone = elementCount(milestoneElements, "deployed")
 
   const progressDone = (elementsCountDone / elementsCount) * 100;
   const progressInProgress = (elementsCountInProgress / elementsCount) * 100;
 
 
-  return (<div className="flex">
-    <div className="h-5 bg-white" style={{width: progressDone + '%'}}></div>
-    <div className="h-5 bg-green" style={{width: progressInProgress + '%'}}></div>
+  return (<div className="flex rounded-xl overflow-hidden p-0.5 bg-white">
+    <div className="h-2 bg-green rounded-xl" style={{width: progressDone + '%'}}></div>
+    <div className="h-2 bg-white" style={{width: progressInProgress + '%'}}></div>
   </div>)
 }
 
@@ -44,14 +46,34 @@ function milestoneName(name: string) {
   if (name == "orphans_past") {
     title = "Previous Tasks";
   } else if (name == "orphans_future") {
-    title = "Future Tasks";
+    title = "Future";
   }
   return title;
 }
 
+
+
 function indexToColor(index: number, total: number) {
   const hue = (index / total) * 360;
-  return `oklch(.3, .1, ${hue})`;
+  const colors = ['#f15a24', '#fbb03b', '#ed1e79', '#4b19d6', '#29abe2', '#79d11c'];
+  const colorPairs = [
+    [colors[0], colors[1]],
+    [colors[1], colors[2]],
+    [colors[2], colors[3]],
+    [colors[3], colors[4]],
+    [colors[4], colors[5]],
+    [colors[1], colors[0]],
+    [colors[2], colors[1]],
+    [colors[3], colors[2]],
+    [colors[4], colors[3]],
+    [colors[5], colors[4]],
+  ];
+  const relativeIndex = index / total;
+  const closestPair = colorPairs[Math.floor(relativeIndex * colorPairs.length)];
+  const [color1, color2] = closestPair;
+
+  return `linear-gradient(-315deg, ${color1}, ${color2})`;
+  return `linear-gradient(-45deg, oklch(40%, .15, ${hue}), oklch(20%, .15, ${hue}))`;
 }
 
 const RoadmapPage: React.FC = () => {
@@ -74,7 +96,6 @@ const RoadmapPage: React.FC = () => {
       title="Roadmap"
       description="This roadmap shows the status of many projects across the Internet Computer stack, but not all - more to come over the next few weeks."
       editPath="https://github.com/dfinity/portal/tree/master/roadmap"
-      
     >
       <ShareMeta image="/img/shareImages/share-roadmap.jpeg"></ShareMeta>
 
@@ -95,23 +116,30 @@ const RoadmapPage: React.FC = () => {
           {data.map((theme, indexTheme) => (
             <article key={theme.name}>
               <h1 className="tw-heading-4 uppercase">{theme.name}</h1>
-              <p className="tw-paragraph">{theme.description}</p>
+
+              <p className="tw-paragraph">
+                {theme.description}
+              </p>
               <section 
                 aria-label="milestones"
-                className="flex gap-8 items-stretch overflow-x-auto scrollbar-hide mt-8 pb-8"
+                className="flex gap-6 items-stretch overflow-x-auto scrollbar-hide mt-8 pb-8"
               >
                 {theme.milestones.map((milestone, index) => (
                   milestone.elements.length > 0 && (
                     <article 
                       key={milestone.name}
-                      className={`text-white rounded-md w-64 basis-64 shrink-0 grow-0 p-8`}
+                      className="text-white rounded-md w-64 basis-64 shrink-0 grow-0 p-6 flex flex-col"
                       style={{
-                        backgroundColor: indexToColor(indexTheme, data.length),
+                        background: indexToColor(indexTheme, data.length),
                       }}
+                      onClick={() => openOverlay(index)}
                     >
-                      <h2>{milestone.metal == 'none' ? milestoneName(milestone.name) : milestone.metal}</h2>
-                      {milestoneElementsToProgress(milestone.elements)}
-                      <p>{milestoneName(milestone.name)}</p>
+                      <div className="grow">
+                        <h2 className="mb-0">{milestone.metal == 'none' ? milestoneName(milestone.name) : milestone.metal}</h2>
+                        <p className="text-xs">{milestone.eta != 'none' ? <span><span className="opacity-35">Milestone</span> {milestone.eta}</span> : <span>&nbsp;</span>}</p>
+                        {milestoneElementsToProgress(milestone.elements)}
+                      </div>
+                      <p className="mb-0 mt-5">{milestoneName(milestone.name)}</p>
                     </article>
                   )
                 ))}
