9c6b0d736d3d6e10e155c0334553063f850fa973
reigj1 <rei.gjyli@Rei-Gjylis-Mac.local>
2024-05-07 16:35:34 +0200

    overlay update

--- src/components/RoadmapPage/Overlay/index.tsx
+++ src/components/RoadmapPage/Overlay/index.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef } from "react";
 import CommunityIcon from "../../../../static/img/community-icon.svg";
 import ExternalLinkIcon from "../../../../static/img/external-link.svg";
 import { RoadmapDomain, RoadmapItem } from "../RoadmapTypes";
+import LinkArrowLeft from "../../Common/Icons/LinkArrowLeft";
 
 const ItemCard: React.FC<{ item: RoadmapItem; deployed: boolean }> = ({
   item,
@@ -79,6 +80,119 @@ const OverlayGroup: React.FC<{
   );
 };
 
+const deployedIcon = (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="24" height="24" rx="12" fill="#18D0B5" />
+    <path
+      d="M7 11.9994L10.8468 16L17 9.59977L15.4617 8L10.8468 12.8005L8.53829 10.3997L7 11.9994Z"
+      fill="white"
+    />
+  </svg>
+);
+
+const inProgressIcon = (
+  <svg viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0.5" width="24" height="24" rx="12" fill="#F68E5F" />
+    <path
+      d="M12.5 7C13.4889 7 14.4556 7.29324 15.2779 7.84265C16.1001 8.39206 16.741 9.17295 17.1194 10.0866C17.4978 11.0002 17.5969 12.0055 17.4039 12.9755C17.211 13.9454 16.7348 14.8363 16.0355 15.5355C15.3363 16.2348 14.4454 16.711 13.4755 16.9039C12.5055 17.0969 11.5002 16.9978 10.5866 16.6194C9.67295 16.241 8.89206 15.6001 8.34265 14.7779C7.79324 13.9556 7.5 12.9889 7.5 12H9.5C9.5 12.5933 9.67595 13.1734 10.0056 13.6667C10.3352 14.1601 10.8038 14.5446 11.3519 14.7716C11.9001 14.9987 12.5033 15.0581 13.0853 14.9424C13.6672 14.8266 14.2018 14.5409 14.6213 14.1213C15.0409 13.7018 15.3266 13.1672 15.4424 12.5853C15.5581 12.0033 15.4987 11.4001 15.2716 10.8519C15.0446 10.3038 14.6601 9.83524 14.1667 9.50559C13.6734 9.17595 13.0933 9 12.5 9V7Z"
+      fill="white"
+    />
+  </svg>
+);
+
+const futureIcon = (
+  <svg viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0.5" width="24" height="24" rx="12" fill="#29ABE2" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M8.83333 12L5.5 15.3333L7.16667 17L12.1667 12L7.16667 7L5.5 8.66667L8.83333 12Z"
+      fill="white"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M15.3148 12L11.9814 15.3333L13.6481 17L18.6481 12L13.6481 7L11.9814 8.66667L15.3148 12Z"
+      fill="white"
+    />
+  </svg>
+);
+
+const MilestoneCard: React.FC<Element> = ({
+  title,
+  overview,
+  status,
+  forum,
+  wiki,
+  docs,
+  proposal,
+}) => {
+  return (
+    <article className=" px-6 pt-6 pb-7 bg-white rounded-lg text-black">
+      <header className="flex gap-0 justify-between self-stretch">
+        <h5 className="tw-heading-6">{title}</h5>
+        <div>
+          {status === "deployed" && deployedIcon}
+          {status === "in_progress" && inProgressIcon}
+          {status === "future" && futureIcon}
+        </div>
+      </header>
+      <p className="tw-paragraph-sm line-clamp-4 ">{overview}</p>
+      <div className="flex gap-2 pr-20 mt-16"></div>
+    </article>
+  );
+};
+
+type Element = {
+  title: string;
+  overview: string;
+  status: string;
+  forum: string;
+  wiki: string;
+  docs: string;
+  proposal: string;
+};
+
+const MilestoneDetail: React.FC<{
+  name: string;
+  overview: string;
+  eta: string;
+  elements: Element[];
+}> = ({ name, overview, eta, elements }) => {
+  return (
+    <article className="border border-white/30 border-solid rounded-xl mb-30">
+      <div className="p-5">
+        <h4 className="tw-heading-4">{name.toUpperCase()}</h4>
+        <p className="tw-paragraph">
+          <span className="text-white/60">Milestone</span> {eta}
+        </p>
+        <div className=" ">
+          <span className="px-2 py-1 w-4 text-sm leading-5 bg-white rounded-xl text-infinite">
+            {elements.length - 1}
+          </span>
+          <span className="text-white"> Completed topics</span>
+        </div>
+        <p className="text-white/60 tw-paragraph mt-6">{overview}</p>
+      </div>
+      <div className="m-1">
+        <div className="grid gap-1 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+          {elements.map((element) => (
+            <MilestoneCard
+              title={element.title}
+              overview={element.overview}
+              status={element.status}
+              forum={element.forum}
+              wiki={element.wiki}
+              docs={element.docs}
+              proposal={element.proposal}
+            />
+          ))}
+        </div>
+      </div>
+    </article>
+  );
+};
+
 const Overlay: React.FC<{
   onClose: () => void;
   openAt: number;
@@ -103,7 +217,7 @@ const Overlay: React.FC<{
 
   return (
     <motion.div
-      className="fixed inset-0 overflow-auto bg-white-80 z-[2000]"
+      className="fixed inset-0 overflow-auto bg-black/60 z-[2000]"
       onClick={onClose}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
@@ -111,35 +225,38 @@ const Overlay: React.FC<{
       ref={overlayRef}
     >
       <div
-        className="relative container-10 !px-0 md:px-6 md:py-12 "
+        className="relative container-10 !px-0 md:px-6 md:py-12 text-white bg-black/60"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="text-right pointer-events-none sticky top-6 pr-6 md:top-20 z-10 md:pr-8">
-          <button
-            className="pointer-events-auto right-8 w-10 h-10 rounded-full border-none bg-white-80 backdrop-blur-2xl"
+        <div className="sticky top-6 pr-6 md:top-20 z-10 md:pr-8">
+          <Link
+            className="link-primary text-white cursor-pointer"
             onClick={onClose}
           >
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8 6.66688L12.6669 2L14 3.33312L9.33312 8L14 12.6669L12.6669 14L8 9.33312L3.33312 14L2 12.6669L6.66688 8L2 3.33312L3.33312 2L8 6.66688Z"
-                fill="#181818"
-              />
-            </svg>
-          </button>
-          <div className="text-black">
-            {data[openAt].name} <br />
-            {anchor !== null &&
-              data[openAt] &&
-              data[openAt].milestones &&
-              data[openAt].milestones[anchor] &&
-              data[openAt].milestones[anchor].name}
-          </div>
+            <LinkArrowLeft /> Back to Roadmap
+          </Link>
+          {data && data[openAt] && (
+            <div>
+              <section className="my-24">
+                <h2 className="tw-heading-2 w-8/10">
+                  {data[openAt].name.toUpperCase()}
+                </h2>
+                <p className="tw-lead w-5/10">{data[openAt].description}</p>
+              </section>
+              <section>
+                {data[openAt].milestones.map((milestone) => {
+                  return (
+                    <MilestoneDetail
+                      name={milestone.name}
+                      overview={milestone.description}
+                      eta={milestone.eta}
+                      elements={milestone.elements}
+                    />
+                  );
+                })}
+              </section>
+            </div>
+          )}
         </div>
       </div>
     </motion.div>
