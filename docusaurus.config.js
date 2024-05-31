7960a390b8418d9be1ac8468f58c27c559fb077b
reigj1 <rei.gjyli@Rei-Gjylis-Mac.local>
2024-05-31 15:09:39 +0200

    update samples

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -203,7 +203,7 @@ const marketingNav = {
               description: "Transforming the internet",
             },
             {
-              name: "ICP - More than a Bitcoin L2",
+              name: "IICP as a Bitcoin L2",
               href: "/bitcoin-integration",
               description: "Bringing smart contracts to Bitcoin",
             },
