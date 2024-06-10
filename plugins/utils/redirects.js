55bb25bb45c31d6e3627f1c43bd44fc2b78d629d
Dominic Wörner <dominic.woerner@dfinity.org>
2024-06-10 19:34:47 +0200

    update tecdsa docs (#3050)
    * update tecdsa docs
    
    Signed-off-by: Dominic Woerner <dominic.woerner@dfinity.org>
    Co-authored-by: Jessie Mongeon <133128541+jessiemongeon1@users.noreply.github.com>

--- plugins/utils/redirects.js
+++ plugins/utils/redirects.js
@@ -86,6 +86,7 @@ const redirects = `
   /docs/current/developer-docs/build/cdks/motoko-dfinity/* /docs/current/motoko/main/:splat
   /docs/current/developer-docs/build/cdks/cdk-rs-dfinity/*  /docs/current/developer-docs/backend/rust/:splat
   /docs/current/developer-docs/build/languages/motoko/* /docs/current/motoko/main/:splat
+  /docs/current/developer-docs/build/languages/motoko/* /docs/current/motoko/main/:splat
 
   /docs/developers-guide/ /docs/current/motoko/main/getting-started/motoko-introduction
   /docs/developers-guide/about-this-guide /docs/current/motoko/main/getting-started/motoko-introduction
@@ -517,6 +518,7 @@ const redirects = `
   /docs/current/developer-docs/web-apps/user-login/email-password /docs/current/developer-docs/identity/authentication/email-password
   /docs/current/developer-docs/multi-chain/ethereum/using-eth/siwe /docs/current/developer-docs/identity/authentication/siwe
   /docs/current/developer-docs/build/cdks/motoko-dfinity/motoko/ /docs/current/motoko/main/getting-started/motoko-introduction
+  /docs/current/developer-docs/smart-contracts/encryption/signing-transactions /docs/current/developer-docs/smart-contracts/encryption/signing-messages
   `
   .split(/[\r\n]+/)
   .map((line) => line.trim().replace(/^#.*$/, "").trim())
