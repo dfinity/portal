c023dd8422769497e8b6ddee4c781c57b4bf1d40
Dominic Woerner <dominic.woerner@dfinity.org>
2024-06-18 14:02:36 +0200

    more details on btc dev workflow
    Signed-off-by: Dominic Woerner <dominic.woerner@dfinity.org>

--- sidebars.js
+++ sidebars.js
@@ -821,6 +821,7 @@ const sidebars = {
             "developer-docs/multi-chain/bitcoin/using-btc/btc-dev-workflow",
             "developer-docs/multi-chain/bitcoin/using-btc/read-state",
             "developer-docs/multi-chain/bitcoin/using-btc/generate-addresses",
+            "developer-docs/multi-chain/bitcoin/using-btc/create-transactions",
             "developer-docs/multi-chain/bitcoin/using-btc/sign-transactions",
             "developer-docs/multi-chain/bitcoin/using-btc/submit-transactions",
             "developer-docs/multi-chain/bitcoin/using-btc/local-development",
