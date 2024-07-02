1d96acf25500bb424e3335706651a6a6fecb1e52
David Aerne <meodai@gmail.com>
2024-07-02 12:39:27 +0200

    optimize assets

--- src/pages/chainfusion.tsx
+++ src/pages/chainfusion.tsx
@@ -1,29 +1,19 @@
 import Link from "@docusaurus/Link";
 import DarkHeroStyles from "@site/src/components/Common/DarkHeroStyles";
-import Card from "@site/src/components/SamplesPage/Card";
 import transitions from "@site/static/transitions.json";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
 import { motion, AnimatePresence } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
 import AnimateSpawn from "../components/Common/AnimateSpawn";
-import { CardWithDescription } from "../components/Common/Card";
 import LinkArrowRight from "../components/Common/Icons/LinkArrowRight";
-import IntraPageNav from "../components/Common/IntraPageNav";
-import { sampleItems } from "../components/Common/sampleItems";
 import ShareMeta from "../components/Common/ShareMeta";
-import CodeBlockString from "../theme/CodeBlock/Content/String";
-import { unreachable } from "../utils/unreachable";
 import { useDarkHeaderInHero } from "../utils/use-dark-header-in-hero";
 import { useScrollSpyMenu } from "../utils/use-scroll-spy-menu";
-import LinkArrowUpRight from "../components/Common/Icons/LinkArrowUpRight";
-import LinkArrowUp from "../components/Common/Icons/LinkArrowUp";
-import LinkArrowDown from "../components/Common/Icons/LinkArrowDown";
 import Tabs from "@theme/Tabs";
 import TabItem from "@theme/TabItem";
 import News from "../components/BitcoinIntegrationPage/News";
 import Newsletter from "../components/Common/Newsletter/Newsletter";
-import BenefitsText from "/img/chainfusion/benefits-text.svg";
 import CodeSnippet from "../components/CodeSnippet";
 import Coins from "/img/chainfusion/coins.svg";
 
@@ -393,7 +383,9 @@ function ChainFusion() {
             variants={transitions.container}
             el={motion.section}
           >
+            <div className="-mt-24 overflow-hidden relative h-[280px] md:h-[560px]">
             <Coins className="overflow-visible" />
+            </div>
           </AnimateSpawn>
           <AnimateSpawn
             className="container-8 py-20 md:py-30 "
