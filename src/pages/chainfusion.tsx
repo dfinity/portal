5cec738b21fa3923611846edeb6bc9c0866c76b8
reigj1 <rei.gjyli@dfinity.org>
2024-09-02 15:16:21 +0200

    update chainfusion images

--- src/pages/chainfusion.tsx
+++ src/pages/chainfusion.tsx
@@ -1727,7 +1727,7 @@ function ChainFusion() {
         <section className="container-12 block md:flex gap-4 mt-24 md:mt-32">
           <article className="basis-1/2 bg-black rounded-[32px] text-white overflow-clip mt-6">
             <div className="md:flex-row items-stretch	relative z-1">
-              <div className="p-1/10 md:p-2/12 py-1/10 pb-64 md:pb-96">
+              <div className="p-1/10 md:p-2/12 py-1/10 pb-64 md:pb-[26rem]">
                 <h5 className="tw-heading-4 md:tw-heading-3">Hello, Bitcoin</h5>
                 <p className="tw-paragraph-sm md:tw-paragraph mt-6">
                   BUILD on Bitcoin at the speed of ICP. Bitcoin on ICP is not
@@ -1752,7 +1752,7 @@ function ChainFusion() {
 
           <article className="basis-1/2 bg-black rounded-[32px] text-white overflow-clip mt-6">
             <div className="md:flex-row items-stretch	relative z-1">
-              <div className="p-1/10 md:p-2/12 py-1/10 pb-64 md:pb-96">
+              <div className="p-1/10 md:p-2/12 py-1/10 pb-64  md:pb-[26rem]">
                 <h5 className="tw-heading-4 md:tw-heading-3">GM Ethereum</h5>
                 <p className="tw-paragraph-sm md:tw-paragraph mt-6">
                   Full support for Ethereum and other EVM chains is now live
