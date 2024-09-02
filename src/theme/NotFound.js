378832e83b1dda81449a4b5b481c1fa529469c85
reigj1 <rei.gjyli@dfinity.org>
2024-09-02 16:59:30 +0200

    remove temp loading

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -4,17 +4,8 @@ import { trackEvent } from "../utils/matomo";
 import CustomNotFoundPage from "../pages/notfound";
 
 export default function NotFoundWrapper(props) {
-  const [isLoaded, setIsLoaded] = useState(false);
-
   useEffect(() => {
     trackEvent("404", window.location.pathname);
-
-    // Simulate initialization process
-    const timer = setTimeout(() => {
-      setIsLoaded(true);
-    }, 100); // adjust this timeout as needed
-
-    return () => clearTimeout(timer);
   }, []);
   return (
     <>
