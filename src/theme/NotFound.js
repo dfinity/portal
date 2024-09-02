2fa19bed21f3ff5af01134b3427051eb9cbbd041
reigj1 <rei.gjyli@dfinity.org>
2024-09-02 16:16:10 +0200

    test inizialization

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -1,11 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NotFound from "@theme-original/NotFound";
 import { trackEvent } from "../utils/matomo";
 import CustomNotFoundPage from "../pages/notfound";
 
 export default function NotFoundWrapper(props) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
     trackEvent("404", window.location.pathname);
+
+    // Simulate initialization process
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 100); // adjust this timeout as needed
+
+    return () => clearTimeout(timer);
   }, []);
   return (
     <>
