e477c1b84f04cc6fbd21b06b772c3b942b876b0a
reigj1 <rei.gjyli@dfinity.org>
2024-09-03 14:25:56 +0200

    test loading scripts

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -1,15 +1,37 @@
-import React, { useEffect } from "react";
-import NotFound from "@theme-original/NotFound";
-import { trackEvent } from "../utils/matomo";
+import React, { useEffect, useState } from "react";
+import { Link } from "@docusaurus/router";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import ChainFusion from "../pages/chainfusion";
 import CustomNotFoundPage from "../pages/notfound";
 
-export default function NotFoundWrapper(props) {
+export default function NotFound() {
+  const context = useDocusaurusContext();
+  const [isClient, setIsClient] = useState(false);
+
   useEffect(() => {
-    trackEvent("404", window.location.pathname);
+    setIsClient(true);
+
+    if (ExecutionEnvironment.canUseDOM) {
+      const style = document.createElement("style");
+      style.textContent = " ";
+      document.head.appendChild(style);
+      requestAnimationFrame(() => document.head.removeChild(style));
+    }
+
+    if (ExecutionEnvironment.canUseDOM) {
+      console.log(
+        "Loaded stylesheets:",
+        Array.from(document.styleSheets)
+          .map((sheet) => sheet.href)
+          .filter(Boolean)
+      );
+    }
   }, []);
-  return (
-    <>
-      <CustomNotFoundPage />
-    </>
-  );
+
+  if (!isClient) {
+    return <div></div>;
+  }
+
+  return <CustomNotFoundPage />;
 }
