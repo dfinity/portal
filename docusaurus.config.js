d0bc52e9afeb1d02ec1cdc9872962a2ab52bd33f
reigj1 <rei.gjyli@dfinity.org>
2024-09-05 16:19:45 +0200

    fix ai modal opening multiple times

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -585,7 +585,24 @@ const config = {
   markdown: {
     mermaid: true,
   },
-  scripts: [],
+  scripts: [
+    {
+      src: "https://widget.kapa.ai/kapa-widget.bundle.js",
+      "data-website-id": "08910249-851f-465b-b60f-238d84e1afc1",
+      "data-project-name": "Internet Computer",
+      "data-project-color": "#172234",
+      "data-project-logo":
+        "https://s3.coinmarketcap.com/static-gravity/image/2fb1bc84c1494178beef0822179d137d.png",
+      "data-modal-override-open-class": "ask-ai-widget-trigger",
+      "data-modal-ask-ai-input-placeholder":
+        "Ask me a question about the Internet Computer Protocol",
+      "data-modal-example-questions":
+        "What is the ICP token?, How is the Internet Computer governed?, How do I start building fully on-chain Web3?",
+      "data-modal-disclaimer":
+        " This LLM provides responses are generated automatically and may be inaccurate or outdated. Please take care to verify or validate any responses before making any critical decisions.",
+      async: true,
+    },
+  ],
   plugins: [
     "docusaurus-plugin-sass",
     customWebpack,
