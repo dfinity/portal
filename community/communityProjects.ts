6c2fa45768a3aa69ace38000eecdd697818071e2
Lucas-alarib <71742193+Lucas-alarib@users.noreply.github.com>
2024-09-16 16:30:12 -0300

    add_dworld

--- community/communityProjects.ts
+++ community/communityProjects.ts
@@ -2067,6 +2067,24 @@ const communityProjects: SampleItem[] = [
     contentType: ["code samples", "community repo", "live demos"],
     image: "/img/community-projects/d-vote.png",
   },
+  {
+    index: 127,
+    title: "Digital World",
+    body: "Digital World Project, a secure platform that integrates decentralized social networking, DeFi services, asset tokenization, real estate registry digitization, and project incubation. It offers e-commerce, chat, digital wallets, personalized profiles, asset conversion, and secure data storage, fostering an efficient ecosystem.",
+    links: {
+      github: "https://github.com/ASPPIBRA-DAO",
+      livePreview: "https://dworld.app",
+      otherLinks: ["https://x.com/ASPPIBRA_ORG",
+        "https://www.youtube.com/@ASPPIBRA-DAO/videos"],
+    },
+    domains: ["Website / Asynchronous DeFi /SocialFi "],
+    level: "advanced",
+    languages: ["python", "javascript"],
+    contentType: ["code samples", "community repo", "live demos"],
+    image: "/img/showcase/dworld_logo.webp",
+  },
+
+
 ];
 
 export default communityProjects;
