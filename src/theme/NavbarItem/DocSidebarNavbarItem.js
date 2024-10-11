23863dad74c8a62b6118b055fc3da9b1494e77bb
reigj1 <rei.gjyli@dfinity.org>
2024-10-11 14:27:16 +0200

    add timeout

--- src/theme/NavbarItem/DocSidebarNavbarItem.js
+++ src/theme/NavbarItem/DocSidebarNavbarItem.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useCallback } from "react";
 import DocSidebarNavbarItemOriginal from "@theme-original/NavbarItem/DocSidebarNavbarItem";
 import { useNavbarMobileSidebar } from "@docusaurus/theme-common/internal";
 
 export default function DocSidebarNavbarItem(props) {
   const mobileSidebar = useNavbarMobileSidebar();
   
-  const handleClick = (e) => {
-    if (window.innerWidth <= 996 && !mobileSidebar.shown) {
-      mobileSidebar.toggle();
+  const handleClick = useCallback((e) => {
+    console.log('click');
+    if (window.innerWidth <= 996) {
+      setTimeout(() => {
+        mobileSidebar.toggle();
+      }, 10);
     }
 
     // Call the original onClick if it exists
     if (props.onClick) {
       props.onClick(e);
     }
-  };
+  }, [mobileSidebar]);
 
   return <DocSidebarNavbarItemOriginal {...props} onClick={handleClick} />;
 }
