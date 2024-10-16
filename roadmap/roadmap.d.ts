0b09242f7dc7b96887199034eb52e44e47f45b59
Andrew <andris788@gmail.com>
2024-10-16 14:35:13 +0200

    Added documentation route

--- roadmap/roadmap.d.ts
+++ roadmap/roadmap.d.ts
@@ -8,7 +8,7 @@ interface Milestone {
   name?: string | string;
   milestone_id: string;
   description: string;
-  eta: null | string;
+  eta: null | string | string;
   status?: string;
   elements: Element[];
 }
