fbbda08049799966103fc12c40d6aeb211b50b37
reigj1 <rei.gjyli@dfinity.org>
2024-11-12 19:41:30 +0100

    update texts

--- src/components/DocsHome/index.tsx
+++ src/components/DocsHome/index.tsx
@@ -46,7 +46,7 @@ const languagesTiles: TileDescriptor[] = [
   {
     label: "Motoko",
     description:
-      "Get started with a high level programming language designed specifically for the Internet Computer",
+      "Get started with an easy to learn domain-specific language that is able to leverage ICP features and is also perfect to let AI code solo when desired. This is a great choice for beginners.",
     icon: (
       <img
         src="/img/docs/motoko.webp"
@@ -92,7 +92,7 @@ const languagesTiles: TileDescriptor[] = [
   {
     label: "Typescript",
     description:
-      "Azle allows you to build Internet Computer apps using TypeScript and JavaScript, the languages of the web",
+      "Code in the language of the web, TypeScript and JavaScript, using Azle.",
     icon: (
       <img
         src="/img/docs/typescript.webp"
@@ -115,7 +115,7 @@ const languagesTiles: TileDescriptor[] = [
   {
     label: "Solidity",
     description:
-      "Bitfinity allows you to deploy smart contracts on the Internet Computer written in Solidity, a popular language for DeFi applications",
+      "Deploy Solidity smart contracts on the Internet Computer with Bitfinity.",
     icon: (
       <img
         src="/img/docs/solidity.webp"
@@ -560,39 +560,13 @@ const DocsHomePage: FC = () => {
               media, games, enterprise apps, AI, Web3, DeFi and…
             </div>
             <div className="tw-paragraph text-white-80 flex flex-col gap-2">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M3 12C3 10.067 4.567 8.5 6.5 8.5C7.7035 8.5 8.51959 8.9338 9.19914 9.61336C9.9255 10.3397 10.4851 11.3322 11.1258 12.4856L11.1595 12.5462C11.7605 13.6283 12.4431 14.8573 13.3866 15.8009C14.3946 16.8088 15.7035 17.5 17.5 17.5C20.5376 17.5 23 15.0376 23 12C23 8.96243 20.5376 6.5 17.5 6.5C15.8394 6.5 14.3508 7.2359 13.3423 8.39937C13.7887 9.05406 14.1574 9.70577 14.464 10.2574C15.0681 9.20718 16.2014 8.5 17.5 8.5C19.433 8.5 21 10.067 21 12C21 13.933 19.433 15.5 17.5 15.5C16.2965 15.5 15.4804 15.0662 14.8009 14.3866C14.0745 13.6603 13.5149 12.6678 12.8742 11.5144L12.8405 11.4538C12.2395 10.3717 11.5569 9.14265 10.6134 8.19914C9.60541 7.1912 8.2965 6.5 6.5 6.5C3.46243 6.5 1 8.96243 1 12C1 15.0376 3.46243 17.5 6.5 17.5C8.16056 17.5 9.64923 16.7641 10.6577 15.6006C10.2113 14.9459 9.84262 14.2942 9.53605 13.7426C8.93194 14.7928 7.79856 15.5 6.5 15.5C4.567 15.5 3 13.933 3 12Z"
-                  fill="white"
-                  fill-opacity="0.8"
-                />
-              </svg>
+              The Internet Computer (ICP) provides a 100% decentralized platform
+              where devs can build without Big Tech and traditional IT.
+            </div>
+            <div className="tw-paragraph text-white-80 flex flex-col gap-2">
               End-to-end decentralization makes services immune to cyber attack,
               unstoppable, censorship-resistant, and optionally tokenized and
               autonomous.
-            </div>
-            <div className="tw-paragraph text-white-80 flex flex-col gap-2">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M3 12C3 10.067 4.567 8.5 6.5 8.5C7.7035 8.5 8.51959 8.9338 9.19914 9.61336C9.9255 10.3397 10.4851 11.3322 11.1258 12.4856L11.1595 12.5462C11.7605 13.6283 12.4431 14.8573 13.3866 15.8009C14.3946 16.8088 15.7035 17.5 17.5 17.5C20.5376 17.5 23 15.0376 23 12C23 8.96243 20.5376 6.5 17.5 6.5C15.8394 6.5 14.3508 7.2359 13.3423 8.39937C13.7887 9.05406 14.1574 9.70577 14.464 10.2574C15.0681 9.20718 16.2014 8.5 17.5 8.5C19.433 8.5 21 10.067 21 12C21 13.933 19.433 15.5 17.5 15.5C16.2965 15.5 15.4804 15.0662 14.8009 14.3866C14.0745 13.6603 13.5149 12.6678 12.8742 11.5144L12.8405 11.4538C12.2395 10.3717 11.5569 9.14265 10.6134 8.19914C9.60541 7.1912 8.2965 6.5 6.5 6.5C3.46243 6.5 1 8.96243 1 12C1 15.0376 3.46243 17.5 6.5 17.5C8.16056 17.5 9.64923 16.7641 10.6577 15.6006C10.2113 14.9459 9.84262 14.2942 9.53605 13.7426C8.93194 14.7928 7.79856 15.5 6.5 15.5C4.567 15.5 3 13.933 3 12Z"
-                  fill="white"
-                  fill-opacity="0.8"
-                />
-              </svg>
-              The Internet Computer (ICP) provides a 100% decentralized platform
-              where devs can build without Big Tech and traditional IT.
             </div>
           </div>
         </div>
@@ -606,9 +580,8 @@ const DocsHomePage: FC = () => {
           <div>
             <h2 className="tw-heading-4 md:tw-heading-3">Languages</h2>
             <p className="tw-paragraph">
-              Explore coding, from beginner to pro, with our comprehensive
-              guides, tutorials, samples, and API docs for Rust, Motoko, and
-              TypeScript
+              Beginner or pro, our comprehensive guides, tutorials, samples, and
+              API docs have you covered.
             </p>
             <Link
               className="button-primary rounded-2xl"
