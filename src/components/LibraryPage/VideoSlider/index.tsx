768e1a134f840d63b8038c6f2f2386b28572e427
Patrick Müller <patrick@parallactic.ch>
2025-01-30 16:31:16 +0100

    update library page title with new dark design

--- src/components/LibraryPage/VideoSlider/index.tsx
+++ src/components/LibraryPage/VideoSlider/index.tsx
@@ -50,18 +50,21 @@ export const CardWithImage: React.FC<{
       to={href}
       className="text-black hover:no-underline"
     >
-      <article className="bg-white rounded-xl flex flex-col justify-start gap-5 group relative overflow-hidden">
+      <article className="flex flex-col justify-start gap-3 group relative overflow-hidden">
+        
         <div
-          className="w-100 aspect-[16/9] bg-gray-200 bg-cover bg-no-repeat bg-center flex items-center justify-center"
+          className="rounded-xl w-100 aspect-[16/9] bg-gray-200 bg-cover bg-no-repeat bg-center flex items-center justify-center"
           style={{ backgroundImage: `url(${image})` }}>
             <svg className="backdrop-blur-lg rounded-full" width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M0 32C0 14.3269 14.3269 0 32 0C49.6731 0 64 14.3269 64 32C64 49.6731 49.6731 64 32 64C14.3269 64 0 49.6731 0 32Z" fill="white" fill-opacity="0.3"/>
               <path d="M39.376 32.4161L28.7774 39.4818C28.5476 39.635 28.2372 39.5729 28.084 39.3432C28.0292 39.261 28 39.1645 28 39.0658V24.9343C28 24.6582 28.2239 24.4343 28.5 24.4343C28.5987 24.4343 28.6952 24.4635 28.7774 24.5183L39.376 31.584C39.6057 31.7372 39.6678 32.0477 39.5146 32.2774C39.478 32.3323 39.4309 32.3795 39.376 32.4161Z" fill="white"/>
             </svg>
         </div>
-        <div className="p-5 pt-0">
+
+        <div className="p-2 pt-0">
           {children}
         </div>
+
       </article>
       
     </MotionLink>
@@ -79,7 +82,7 @@ export const VideoSlider = () => {
   const settings = {
     dots: false,
     infinite: false,
-    slidesToShow: 3.5,
+    slidesToShow: 2.5,
     slidesToScroll: 1,
     autoplay: false,
     autoplaySpeed: 2000,
@@ -112,12 +115,26 @@ export const VideoSlider = () => {
   };
   return (
     <div className="slider-container">
+
+      <aside className="gap-4 justify-end mb-6 hidden md:flex">
+        <button className="bg-transparent w-12 h-12 p-0 inline-flex justify-center items-center rounded-full border border-white-20 border-solid outline-none" onClick={previous} aria-label="Previous">
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3.82843 6.9999H16V8.9999H3.82843L9.1924 14.3638L7.7782 15.778L0 7.9999L7.7782 0.22168L9.1924 1.63589L3.82843 6.9999Z" fill="#AE9EFF"/>
+          </svg>
+        </button>
+        <button className="bg-transparent w-12 h-12 p-0 inline-flex justify-center items-center rounded-full border border-white-20 border-solid outline-none" onClick={next} aria-label="Next">
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.1716 6.9999L6.8076 1.63589L8.2218 0.22168L16 7.9999L8.2218 15.778L6.8076 14.3638L12.1716 8.9999H0V6.9999H12.1716Z" fill="#AE9EFF"/>
+          </svg>
+        </button>
+      </aside>
+
       <Slider ref={(slider) => (sliderRef = slider)} {...settings}>
         {sliderData.map((data, index) => (
           <div key={index} className="border-box pr-6">
             <CardWithImage href={data.link} image={data.image}>
-              <div className="h-16">
-                <h1 className="tw-heading-7 md:tw-heading-5 mb-0 line-clamp-2">
+              <div className="h-8">
+                <h1 className="tw-heading-7 md:tw-heading-7 mb-0 line-clamp-1 text-white">
                   {data.title}
                 </h1>
               </div>
@@ -125,18 +142,7 @@ export const VideoSlider = () => {
           </div>
         ))}
       </Slider>
-      <aside className="gap-4 justify-end mt-6 hidden md:flex">
-        <button className="bg-transparent w-12 h-12 p-0 inline-flex justify-center items-center rounded-full border border-black-20 outline-none" onClick={previous} aria-label="Previous">
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3.82843 6.9999H16V8.9999H3.82843L9.1924 14.3638L7.7782 15.778L0 7.9999L7.7782 0.22168L9.1924 1.63589L3.82843 6.9999Z" fill="#3B00B9"/>
-          </svg>
-        </button>
-        <button className="bg-transparent w-12 h-12 p-0 inline-flex justify-center items-center rounded-full border border-black-20 outline-none" onClick={next} aria-label="Next">
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12.1716 6.9999L6.8076 1.63589L8.2218 0.22168L16 7.9999L8.2218 15.778L6.8076 14.3638L12.1716 8.9999H0V6.9999H12.1716Z" fill="#3B00B9"/>
-          </svg>
-        </button>
-      </aside>
+      
     </div>
   );
 };
