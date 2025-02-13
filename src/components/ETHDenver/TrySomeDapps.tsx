9b156406d73071b3b00f12b741f74644c842082e
Patrick Müller <patrick@parallactic.ch>
2025-02-13 16:25:47 +0100

    add showcase apps and filter the list with featured projects

--- src/components/ETHDenver/TrySomeDapps.tsx
+++ src/components/ETHDenver/TrySomeDapps.tsx
@@ -4,67 +4,24 @@ import AnimateSpawn from "../Common/AnimateSpawn";
 import Link from "@docusaurus/Link";
 import { motion } from "framer-motion";
 import transitions from "@site/static/transitions.json";
+import showcaseData from "../../../showcase.json";
 
-const projects: {
-  logo: string;
-  name: string;
-  description: string;
-  agenda: string[];
-}[] = [
-    {
-      name: "AstroX (me wallet)",
-      logo: "/img/ethdenver/astrox.webp",
-      description:
-        "Multichain wallet securing your assets without seed phrase across any devices.",
-      agenda: ["Mar 2 - 5, 2023"],
-    },
-    {
-      name: "Hot or Not",
-      logo: "/img/ethdenver/hotornot.webp",
-      description: "Monetise Time on Social Media. Video platform in ICP.",
-      agenda: ["Mar 2 - 5, 2023"],
-    },
-    {
-      name: "Finterest",
-      logo: "/img/ethdenver/finterest.webp",
-      description: "The first truly decentralized price feed.",
-      agenda: ["Mar 2 - 5, 2023"],
-    },
-    {
-      name: "Toniq Labs",
-      logo: "/img/ethdenver/toniq.webp",
-      description: "We help you build, launch, and grow NFT-based businesses.",
-      agenda: ["Mar 2 - 5, 2023"],
-    },
-    {
-      name: "ORIGYN",
-      logo: "/img/ethdenver/origyn.webp",
-      description: "Digital verifications for physical objects through NFTs.",
-      agenda: ["Mar 3 / 4, 2023"],
-    },
 
-    {
-      name: "Demergent Labs",
-      logo: "/img/ethdenver/demergent-labs.webp",
-      description: "Accelerating the adoption of Web3 and the Internet Computer.",
-      agenda: ["Mar 3 / 4, 2023"],
-    },
-    {
-      name: "Itoka",
-      logo: "/img/ethdenver/itoka-logo.webp",
-      description:
-        "Become a musician smooth and simple with AI and Web3 technology. Only on ICP.",
-      agenda: ["Mar 3 / 4, 2023", "5:00pm - 6:00pm MST"],
-    },
-
-    {
-      name: "ntagle",
-      logo: "/img/ethdenver/ntagle-logo.webp",
-      description:
-        "Scalable and cost-effective way to trustlessly bind physical objects to canisters",
-      agenda: ["Mar 4, 2023", "8:00am - 10:00am MST"],
-    },
-  ];
+const selectedProjectIds = [
+  'omnitynetwork',
+  'onicai',
+  'motdex',
+  'decideai',
+  'tap-protocol',
+  'bioniq',
+  'origyn',
+  'waterneuron',
+  'icdevs.org',
+  'chainsight',
+  'oisy',
+  'chainkeyx'
+]
+const projects = showcaseData.filter((p) => selectedProjectIds.includes(p.id));
 
 const TrySomeDapps: FC<{
   id: string;
@@ -92,31 +49,29 @@ const TrySomeDapps: FC<{
           className="container-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-5 mt-12"
           variants={transitions.container}
         >
-          {projects.map((p, i) => (
+          {projects?.map((p, i) => (
             <motion.article
               variants={transitions.item}
               key={p.name + i}
-              className="rounded-xl bg-white p-6 md:p-8 no-underline text-black hover:no-underline hover:text-black flex flex-col"
+              className="rounded-xl bg-white p-6 md:p-8 no-underline text-black hover:no-underline hover:text-black flex flex-col "
             >
               <img
                 src={p.logo}
                 alt={p.name}
-                className="w-16 sm:w-20 mb-3 h-16 sm:h-20 object-contain absolute left-6 top-6 sm:static"
+                className="w-16 sm:w-20 mb-3 h-16 sm:h-20 ml-0 object-contain absolute left-6 top-6 sm:static"
               />
-              <div className="ml-[86px] sm:ml-0 flex-1 flex flex-col">
-                <h3 className="tw-heading-6 sm:tw-heading-5 mb-1 sm:mb-2">
-                  {p.name}
-                </h3>
-                <p className="tw-paragraph-sm sm:tw-lead-sm mb-3 sm:mb-4 text-black-60 flex-1">
-                  {p.description}
-                </p>
-                <p className="mb-0 tw-title-navigation text-gradient-base text-gradient-denver">
-                  {p.agenda.map((a) => (
-                    <span key={a} className="inline-block">
-                      {a}
-                    </span>
-                  ))}
-                </p>
+              <div className="ml-[86px] sm:ml-0 mt-4 flex-1 flex flex-col justify-between items-start">
+                <div>
+                  <h3 className="tw-heading-6 sm:tw-heading-5 mb-1 sm:mb-2">
+                    {p.name}
+                  </h3>
+                  <p className="tw-paragraph-sm sm:tw-lead-sm mb-3 sm:mb-4 text-black-60 flex-1">
+                    {p.description}
+                  </p>
+                </div>
+                <Link className="button-outline mt-4" href={p.website}>
+                  Try it
+                </Link>
               </div>
             </motion.article>
           ))}
