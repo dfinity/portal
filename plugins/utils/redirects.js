0e59bf5fa0a93050bdf510153b95d409656d9591
Jessie Mongeon <jessie.mongeon@dfinity.org>
2025-02-20 15:43:11 -0600

    redirects

--- plugins/utils/redirects.js
+++ plugins/utils/redirects.js
@@ -197,7 +197,7 @@ const redirects = `
   /docs/developers-guide/customize-projects /docs/building-apps/developing-canisters/what-is-a-canister
   /docs/developers-guide/default-wallet /docs/building-apps/canister-management/topping-up
   /docs/developers-guide/design-apps /docs/building-apps/essentials/canisters
-  /docs/developers-guide/glossary /docs/references/glossary/
+  /docs/developers-guide/glossary /docs/references/glossary
   /docs/developers-guide/install-upgrade-remove /docs/building-apps/getting-started/install
   /docs/developers-guide/lang-service-ide /docs/building-apps/developer-tools/ide/icp-ninja
   /docs/developers-guide/reinstalling-dfx /docs/building-apps/getting-started/install
@@ -665,7 +665,7 @@ const redirects = `
   /docs/concepts/governance https://internetcomputer.zendesk.com/hc/en-us/articles/33692645961236-NNS-Network-Nervous-System
   /docs/concepts/nodes-subnets /docs/building-apps/developing-canisters/create
   /docs/concepts/subnet-types /docs/building-apps/developing-canisters/create
-  /docs/references/glossary /docs/references/glossary/
+  /docs/references/glossary /docs/references/glossary
   /docs/current/developer-docs/multi-chain/faq/user-faq /docs/building-apps/chain-fusion/overview
   /docs/current/developer-docs/multi-chain/faq/ckbtc-faq /docs/defi/chain-key-tokens/ckbtc/overview
   /docs/current/developer-docs/multi-chain/faq/cketh-faq /docs/defi/chain-key-tokens/cketh/overview
