e6f04c84647432b377c9cc61efd1070d66e783ee
Jessie Mongeon <133128541+jessiemongeon1@users.noreply.github.com>
2025-06-16 13:29:42 -0500

    docs: New Build on BTC dev docs  (#5697)
    * Build on bitcoin init
    
    * Build on BTC
    
    * Build on BTC
    
    * sidebars
    
    * more edits
    
    * build on btc
    
    * Build on btc
    
    * update code snippets
    
    * redirects
    
    * redirects
    
    * redirects
    
    * redirects
    
    * Update docs/build-on-btc/using-regtest.mdx
    
    Co-authored-by: Kristofer <kristofer@fmckl.se>
    
    * Update docs/build-on-btc/btc-dev-env.mdx
    
    Co-authored-by: Kristofer <kristofer@fmckl.se>
    
    * code review feedback
    
    * Update docs/build-on-btc/btc-api.mdx
    
    Co-authored-by: Thomas Locher <thomas.locher@dfinity.org>
    
    * Update docs/build-on-btc/btc-api.mdx
    
    Co-authored-by: Thomas Locher <thomas.locher@dfinity.org>
    
    * Update docs/build-on-btc/btc-api.mdx
    
    Co-authored-by: Thomas Locher <thomas.locher@dfinity.org>
    
    * sidebar redirect for BTC pages
    
    * Update docs/build-on-btc/btc-dev-env.mdx
    
    Co-authored-by: Thomas Locher <thomas.locher@dfinity.org>
    
    * Update docs/build-on-btc/btc-transactions/create-transactions.mdx
    
    Co-authored-by: Thomas Locher <thomas.locher@dfinity.org>
    
    * Update docs/build-on-btc/index.mdx
    
    Co-authored-by: Thomas Locher <thomas.locher@dfinity.org>
    
    * sidebar redirect for BTC pages
    
    * code review suggestions
    
    * code review suggestions
    
    ---------
    
    Co-authored-by: Kristofer <kristofer@fmckl.se>
    Co-authored-by: Thomas Locher <thomas.locher@dfinity.org>

--- src/components/BitcoinIntegrationPage/Videos/index.tsx
+++ src/components/BitcoinIntegrationPage/Videos/index.tsx
@@ -139,7 +139,7 @@ function Index() {
         <br />
         <Link
           className="link-primary link-with-icon md:mt-4"
-          href="/docs/building-apps/chain-fusion/bitcoin/overview"
+          href="/docs/build-on-btc/"
           target="_blank"
           rel="noopener noreferrer"
         >
