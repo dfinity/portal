d9e081f01e71e4336c415e94db48e8b539f60807
Claudio Russo <claudio@dfinity.org>
2025-08-19 18:04:37 +0100

    chore: updateing moc version to 0.16.0

--- static/load_moc.ts
+++ static/load_moc.ts
@@ -1,6 +1,6 @@
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
-const MOC_VERSION = "0.14.3";
+const MOC_VERSION = "0.16.0";
 
 async function addPackage(name, repo, version, dir) {
   const meta_url = `https://data.jsdelivr.com/v1/package/gh/${repo}@${version}/flat`;
@@ -26,7 +26,7 @@ function attachOnRunButton() {
     script.src = `/moc-interpreter-${MOC_VERSION}.js`;
 
     script.addEventListener("load", () => {
-      addPackage("base", "dfinity/motoko-base", `moc-${MOC_VERSION}`, "src")
+      addPackage("core", "dfinity/motoko-core", `moc-${MOC_VERSION}`, "src")
         .then(() => {
           console.log(`moc ${MOC_VERSION} loaded`);
           // Run code
