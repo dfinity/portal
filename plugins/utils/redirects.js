dc510e7f2016ae7c9d8027cbf52bc0f58fe6d398
reigj1 <61514978+reigj1@users.noreply.github.com>
2025-10-02 11:07:23 +0200

    alliance redirect (#6098)

--- plugins/utils/redirects.js
+++ plugins/utils/redirects.js
@@ -1412,7 +1412,7 @@ const redirects = `
   /showcase /ecosystem
   /sustainability /capabilities/sustainability
   /videos /news
-  /community/asia-alliance /
+  /community/asia-alliance https://dfinity.org/alliance
   /docs/search /docs/home
   /docs/security-best-practices/ /docs/building-apps/security/inter-canister-calls
   /docs/security-best-practices/introduction /docs/building-apps/security/overview
