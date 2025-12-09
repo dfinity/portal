2e5ede2d3630979a4eead1000ef114115cf373ef
Marco Walz <marco.walz@dfinity.org>
2025-12-09 10:33:17 +0100

    chore: adding back the CodeBlock theme, not 100% sure if relevant yet

---
+++ src/theme/CodeBlock/hljs_run.js
@@ -0,0 +1,40 @@
+
+export function extractConfig(props) {
+  const name = props.name ? props.name + ".mo" : "stdin";
+  var include = [];
+  if (props.hasOwnProperty("include")) {
+    var split = props.include.split(",");
+    split.forEach((f) => include.push(f));
+  }
+  return {
+    name,
+    include,
+    isRun: props.hasOwnProperty("run"),
+  };
+}
+
+function saveIncluded(include) {
+  var codes = {};
+  include.forEach((id) => {
+    const node = document.getElementById(id);
+    const code = node.innerText;
+    const name = id + ".mo";
+    Motoko.saveFile(name, code);
+    codes[name] = code;
+  });
+  return codes;
+}
+
+export function handleRun({ code, setOutput, setError, config }) {
+  if (typeof Motoko !== "undefined") {
+    const codes = saveIncluded(config.include);
+    const file = config.name;
+    Motoko.saveFile(file, code);
+    const list = config.include.map((s) => s + ".mo");
+    const out = Motoko.run(list, file);
+    setOutput(out.stdout);
+    setError(out.stderr);
+  } else {
+    console.log("moc not found");
+  }
+}
